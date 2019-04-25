21b7d663c567aa52bf9b06b5c19ee65233f387c8
Olga <gorskih.olka@yandex.ru>
2019-04-25 15:45:41 +0300

    Add files

--- tasks/src/main/java/ru/sberbank/school/task02/util/ClientOperation.java
+++ tasks/src/main/java/ru/sberbank/school/task02/util/ClientOperation.java
@@ -1,6 +1,15 @@
 package ru.sberbank.school.task02.util;
 
+/**
+ * Вид операции с валютой.
+ */
 public enum ClientOperation {
+    /**
+     * Клиент хочет купить.
+     */
     BUY,
+    /**
+     * Клиент хочет продать.
+     */
     SELL
 }
