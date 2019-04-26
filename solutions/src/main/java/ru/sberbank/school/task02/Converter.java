8963abb7de1b07f5cc41a27195c53f3b1ccf35c0
w8 <w8@wcsl.ru>
2019-04-26 15:01:14 +0300

    Upload hw task2

---
+++ solutions/src/main/java/ru/sberbank/school/task02/Converter.java
@@ -0,0 +1,64 @@
+package ru.sberbank.school.task02;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import ru.sberbank.school.task02.util.ClientOperation;
+import ru.sberbank.school.task02.util.Quote;
+import ru.sberbank.school.task02.util.Symbol;
+
+public class Converter implements FxConversionService {
+
+    private ExternalQuotesService service;
+
+    public Converter(ExternalQuotesService service) {
+        this.service = service;
+    }
+
+    @Override
+    public BigDecimal convert(ClientOperation operation, Symbol symbol, BigDecimal amount) {
+        List<Quote> quotes = service.getQuotes(symbol);
+        if(amount.compareTo(BigDecimal.ZERO) <= 0 || quotes.size() <= 0){
+            return null;
+        }
+
+        quotes.sort((o1, o2) -> {
+            if (o1.isInfinity()) {
+                return 1;
+            }
+            if (o2.isInfinity()) {
+                return 0;
+            }
+            return o1.getVolumeSize().compareTo(o2.getVolumeSize());
+        });
+
+        Quote matchedQuote = getMatchedQuote(quotes, amount);
+        if(matchedQuote == null){
+            return null;
+        }
+        return operation == ClientOperation.SELL ? matchedQuote.getBid() : matchedQuote.getOffer();
+    }
+
+    private Quote getMatchedQuote(List<Quote> quotes, BigDecimal amount){
+        Quote res = null;
+        for (Quote q : quotes) {
+            if(amount.compareTo(q.getVolumeSize()) < 0){
+                res = q;
+                break;
+            }
+        }
+        if(res == null){
+            res = getInfQuote(quotes);
+        }
+        return res;
+    }
+
+    private Quote getInfQuote(List<Quote> quotes){
+        for (Quote q : quotes) {
+            if(q.isInfinity()){
+                return q;
+            }
+        }
+        return null;
+    }
+}
