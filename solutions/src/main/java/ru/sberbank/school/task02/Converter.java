b0cd2ffee950427f7e605be9da80ef5c3ec31f98
Yuriy <47464282+GWait@users.noreply.github.com>
2019-04-26 05:11:57 +0300

    Upload converter

--- solutions/src/main/java/ru/sberbank/school/task02/Converter.java
+++ solutions/src/main/java/ru/sberbank/school/task02/Converter.java
@@ -1,41 +1,77 @@
 package ru.sberbank.school.task02;
 
-import java.math.BigDecimal;
-import java.util.List;
-
 import ru.sberbank.school.task02.util.ClientOperation;
 import ru.sberbank.school.task02.util.Quote;
 import ru.sberbank.school.task02.util.Symbol;
 
+import java.math.BigDecimal;
+import java.util.Comparator;
+import java.util.List;
+
 public class Converter implements FxConversionService {
 
-    private ExternalQuotesService service;
+    private ExternalQuotesService quotesService;
 
-    public Converter(ExternalQuotesService service) {
-        this.service = service;
+    public Converter(ExternalQuotesService quotesService){
+        this.quotesService = quotesService;
     }
 
     @Override
     public BigDecimal convert(ClientOperation operation, Symbol symbol, BigDecimal amount) {
-        List<Quote> quotes = service.getQuotes(symbol);
-        quotes.sort((o1, o2) -> {
-            if (o1.isInfinity()) {
-                return 1;
-            }
-            if (o2.isInfinity()) {
-                return 0;
-            }
-            return o1.getVolumeSize().compareTo(o2.getVolumeSize());
-        });
-        BigDecimal lastVolume = BigDecimal.ZERO;
+
+        List<Quote> quotes = quotesService.getQuotes(symbol);
+        if(amount.compareTo(BigDecimal.ZERO) <= 0 || quotes.size() <= 0){
+            return null;
+        }
+
         Quote matchedQuote = null;
-        for (Quote quote : quotes) {
-            if (amount.compareTo(lastVolume) > 0) {
-                if (amount.compareTo(quote.getVolumeSize()) < 0) {
-                    matchedQuote = quote;
+
+        quotes.sort(new Comparator<Quote>() {
+            @Override
+            public int compare(Quote o1, Quote o2) {
+                if(o1.isInfinity()){
+                    return 1;
                 }
+                if(o2.isInfinity()){
+                    return 0;
+                }
+                return o1.getVolumeSize().compareTo(o2.getVolumeSize());
+            }
+        });
+
+        matchedQuote = getMatchedQuote(quotes,amount);
+
+        if(matchedQuote != null){
+            if(operation == ClientOperation.BUY){
+                return matchedQuote.getOffer();
+            }else{
+                return matchedQuote.getBid();
             }
         }
-        return operation == ClientOperation.SELL ? matchedQuote.getBid() : matchedQuote.getOffer();
+
+        return null;
+    }
+
+    private Quote getMatchedQuote(List<Quote> quotes, BigDecimal amount){
+        Quote res = null;
+        for (Quote q : quotes) {
+            if(amount.compareTo(q.getVolumeSize()) <= 0){
+                res = q;
+                break;
+            }
+        }
+        if(res == null){
+            res = getInfQuote(quotes);
+        }
+        return res;
+    }
+
+    private Quote getInfQuote(List<Quote> quotes){
+        for (Quote q : quotes) {
+            if(q.isInfinity()){
+                return q;
+            }
+        }
+        return null;
     }
 }
