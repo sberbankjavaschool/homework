2ff046789c4f13fb71b3afd9ff211caf93b3a5ad
Olga <gorskih.olka@yandex.ru>
2019-04-28 16:59:04 +0300

    Add changes

--- solutions/src/main/java/ru/sberbank/school/task02/CurrencyCalc.java
+++ solutions/src/main/java/ru/sberbank/school/task02/CurrencyCalc.java
@@ -5,7 +5,7 @@ import ru.sberbank.school.task02.util.Quote;
 import ru.sberbank.school.task02.util.Symbol;
 
 import java.math.BigDecimal;
-import java.util.Collections;
+import java.math.RoundingMode;
 import java.util.List;
 
 public class CurrencyCalc implements FxConversionService {
@@ -26,6 +26,7 @@ public class CurrencyCalc implements FxConversionService {
         List<Quote> quotes = externalQuotesService.getQuotes(symbol);
         sortQuotes(quotes);
         Quote currentQuote = quotes.get(0);
+        amount = amount.setScale(0, RoundingMode.HALF_UP);
 
         for (Quote quote : quotes) {
             if (amount.compareTo(quote.getVolumeSize()) <= 0 || quote.isInfinity()) {
@@ -38,7 +39,7 @@ public class CurrencyCalc implements FxConversionService {
     }
 
     private void sortQuotes(List<Quote> quotes) {
-        Collections.sort(quotes, (o1, o2) -> {
+        quotes.sort( (o1, o2) -> {
             if (o1.isInfinity()) {
                 return 1;
             }
