e2bfe9070127ead36cac1c78f153a3b196768159
Olga <gorskih.olka@yandex.ru>
2019-04-28 20:04:24 +0300

    Add changes

--- solutions/src/main/java/ru/sberbank/school/task02/CurrencyCalc.java
+++ solutions/src/main/java/ru/sberbank/school/task02/CurrencyCalc.java
@@ -5,7 +5,6 @@ import ru.sberbank.school.task02.util.Quote;
 import ru.sberbank.school.task02.util.Symbol;
 
 import java.math.BigDecimal;
-import java.math.RoundingMode;
 import java.util.List;
 
 public class CurrencyCalc implements FxConversionService {
@@ -18,18 +17,17 @@ public class CurrencyCalc implements FxConversionService {
 
 
     @Override
-    public BigDecimal convert(ClientOperation operation, Symbol symbol, BigDecimal amount) {
-        if (amount == null || operation == null || symbol == null || amount.equals(BigDecimal.ZERO)) {
+    public BigDecimal convert( ClientOperation operation, Symbol symbol, BigDecimal amount) {
+        if (amount == null || operation == null || symbol == null || amount.equals(BigDecimal.ZERO)) {    //да, это чудовищно и я помню про lombok, но почему то у меня не получилось проставить аннотоции
             throw new IllegalArgumentException();
         }
 
         List<Quote> quotes = externalQuotesService.getQuotes(symbol);
         sortQuotes(quotes);
         Quote currentQuote = quotes.get(0);
-        amount = amount.setScale(0, RoundingMode.HALF_UP);
 
         for (Quote quote : quotes) {
-            if (amount.compareTo(quote.getVolumeSize()) <= 0 || quote.isInfinity()) {
+            if (amount.compareTo(quote.getVolumeSize()) < 0 || quote.isInfinity()) {
                 currentQuote = quote;
                 break;
             }
