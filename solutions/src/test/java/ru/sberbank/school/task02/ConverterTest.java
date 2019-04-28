d25cb9e6d1b88ba1a107da3fc314511eaf3f3fd9
w8 <w8@wcsl.ru>
2019-04-28 15:54:17 +0300

    Update JUnit

--- solutions/src/test/java/ru/sberbank/school/task02/ConverterTest.java
+++ solutions/src/test/java/ru/sberbank/school/task02/ConverterTest.java
@@ -1,9 +1,11 @@
 package ru.sberbank.school.task02;
 
 
+
 import java.math.BigDecimal;
 
 import org.junit.jupiter.api.Test;
+
 
 import ru.sberbank.school.task02.util.ClientOperation;
 import ru.sberbank.school.task02.util.ExternalQuotesServiceDemo;
