5ae7892f4d0fc8c69040fe66b93308818e42272a
Gregory <GregoryMelnikov@gmail.com>
2019-04-28 23:58:59 +0300

    task02: new repo

--- solutions/src/main/java/ru/sberbank/school/task02/services/implementation/InternalQuotesServiceImpl.java
+++ solutions/src/main/java/ru/sberbank/school/task02/services/implementation/InternalQuotesServiceImpl.java
@@ -3,11 +3,10 @@ package ru.sberbank.school.task02.services.implementation;
 import lombok.RequiredArgsConstructor;
 import ru.sberbank.school.task02.ExternalQuotesService;
 import ru.sberbank.school.task02.services.InternalQuotesService;
-import ru.sberbank.school.task02.services.exeption.EmptyQuoteList;
+import ru.sberbank.school.task02.exeption.EmptyQuoteList;
 import ru.sberbank.school.task02.util.Quote;
 import ru.sberbank.school.task02.util.Symbol;
 
-import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -23,7 +22,7 @@ public class InternalQuotesServiceImpl implements InternalQuotesService {
      * Получает список котировок от внешнего сервиса, выполняет проверку на наличие элементов в списке и сортировку
      *
      * @param symbol Инструмент
-     * @return TreeMap, ключ - объем, значение - котировка
+     * @return отсортированный список котировок
      */
     @Override
     public List<Quote> getQuotes(Symbol symbol) {
@@ -32,8 +31,6 @@ public class InternalQuotesServiceImpl implements InternalQuotesService {
         if (quotes.isEmpty()) {
             throw new EmptyQuoteList("External quotes service must return at least one quote!");
         }
-
-        List<Quote> quotes2 = new ArrayList<>();
 
         quotes.sort((previos, current) -> {
             int result = previos.getVolumeSize().compareTo(current.getVolumeSize());
