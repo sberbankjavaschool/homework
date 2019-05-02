9ec865f8724a3fea0bcbb1973935778c1510a343
Tanya Naumovets <naumovets.ts@gmail.com>
2019-05-02 23:00:12 +0300

    Change check of volume, checkstyle changes

--- solutions/src/main/java/ru/sberbank/school/task02/Solution.java
+++ solutions/src/main/java/ru/sberbank/school/task02/Solution.java
@@ -14,11 +14,11 @@ public class Solution {
         CalculatorFactory calculatorFactory = new CalculatorFactory();
         // получаем наш валютный калькулятор
         FxConversionService calculator =
-            calculatorFactory.getFxConversionService(new ExternalQuotesServiceDemo());
+                calculatorFactory.getFxConversionService(new ExternalQuotesServiceDemo());
 
         try {
             BigDecimal price = calculator.convert(ClientOperation.BUY,
-                    Symbol.USD_RUB, new BigDecimal(10));
+                    Symbol.USD_RUB, new BigDecimal(100));
             System.out.println(price);
         } catch (WrongSymbolException ex) {
             System.out.println(ex.getMessage());
