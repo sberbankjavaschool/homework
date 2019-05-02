986cf8e200397dff987e3b7b269a650b19b85992
Tanya Naumovets <naumovets.ts@gmail.com>
2019-05-02 20:07:55 +0300

    add @Override and checkstyle changes

--- solutions/src/main/ru/sberbank/school/task02/Solution.java
+++ solutions/src/main/ru/sberbank/school/task02/Solution.java
@@ -1,29 +1,28 @@
 package ru.sberbank.school.task02;
 
+import java.math.BigDecimal;
+
 import ru.sberbank.school.task02.exception.ConverterConfigurationException;
 import ru.sberbank.school.task02.exception.WrongSymbolException;
 import ru.sberbank.school.task02.util.ClientOperation;
 import ru.sberbank.school.task02.util.ExternalQuotesServiceDemo;
 import ru.sberbank.school.task02.util.Symbol;
 
-import java.math.BigDecimal;
-
 public class Solution {
     public static void main(String[] args) {
 
         CalculatorFactory calculatorFactory = new CalculatorFactory();
         // получаем наш валютный калькулятор
-        FxConversionService calculator = calculatorFactory.getFxConversionService(new ExternalQuotesServiceDemo());
-        //TODO добавить ввод с консоли ?
-        //System.out.println(Arrays.toString(ClientOperation.values()));
+        FxConversionService calculator =
+            calculatorFactory.getFxConversionService(new ExternalQuotesServiceDemo());
+
         try {
-            BigDecimal price = calculator.convert(ClientOperation.BUY, Symbol.USD_RUB, new BigDecimal(10));
+            BigDecimal price = calculator.convert(ClientOperation.BUY,
+                    Symbol.USD_RUB, new BigDecimal(10));
             System.out.println(price);
-        }
-        catch (WrongSymbolException ex) {
+        } catch (WrongSymbolException ex) {
             System.out.println(ex.getMessage());
-        }
-        catch (ConverterConfigurationException ex) {
+        } catch (ConverterConfigurationException ex) {
             System.out.println(ex.getMessage());
         }
     }
