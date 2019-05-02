60f7d2a73ebbe45d98d0bdebc0d44756d8dba564
Tanya Naumovets <naumovets.ts@gmail.com>
2019-05-02 22:24:53 +0300

    Small changes

--- solutions/src/main/ru/sberbank/school/task02/CalculatorFactory.java
+++ solutions/src/main/ru/sberbank/school/task02/CalculatorFactory.java
@@ -6,5 +6,9 @@ public class CalculatorFactory implements ServiceFactory {
     public FxConversionService getFxConversionService(ExternalQuotesService externalQuotesService) {
         return new Calculator(externalQuotesService);
     }
-
+    
+    @Override
+    public ExtendedFxConversionService getExtendedFxConversionService(ExternalQuotesService externalQuotesService) {
+        return null;
+    }
 }
