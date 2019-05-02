e9319d57713604fe21e51c8b27db7f6c5f0a5262
aloba <alobanova2144@gmail.com>
2019-05-03 02:07:40 +0300

    ВК ч.1 (id 2). Добавила исключения и исправила ошибки стиля. Попытка №2

--- solutions/src/main/java/ru/sberbank/school/task02/Converter.java
+++ solutions/src/main/java/ru/sberbank/school/task02/Converter.java
@@ -11,39 +11,53 @@ public class Converter implements FxConversionService {
 
     private ExternalQuotesService externalQuotes;
 
-    public Converter(ExternalQuotesService externalQuotesService){
+    public Converter(ExternalQuotesService externalQuotesService) {
         externalQuotes = externalQuotesService;
     }
 
     @Override
     public BigDecimal convert(ClientOperation operation, Symbol symbol, BigDecimal amount) {
         List<Quote> quotes = externalQuotes.getQuotes(symbol);
+
+        if(quotes == null || quotes.isEmpty()) {
+            throw new FxConversionException("No volumes");
+        }
+
         Volume volume = Volume.from(amount);
 
-        if(volume.isInfinity()) throw new FxConversionException("Сумма меньше или равна 0");
+        if (volume.isInfinity()) {
+            throw new FxConversionException("Amount less than or equal to 0");
+        }
 
-        BigDecimal price = new BigDecimal(-1);
+        BigDecimal price;
 
-        switch(operation){
+        switch (operation) {
             case BUY: {
-                price = getSearchQuote(volume, quotes).getOffer();
+                price = searchQuote(volume, quotes).getOffer();
                 break;
             }
             case SELL: {
-                price = getSearchQuote(volume, quotes).getBid();
+                price = searchQuote(volume, quotes).getBid();
                 break;
+            }
+            default: {
+                throw new FxConversionException("Incorrect operation");
             }
         }
 
         return price;
     }
 
-    private Quote getSearchQuote(Volume volume,  List<Quote> quotes){
+    private Quote searchQuote(Volume volume, List<Quote> quotes) {
         Quote searchQuote = null;
         quotes.sort(Comparator.comparing(Quote::getVolumeSize));
 
-        for (Quote quote : quotes){
-            if(volume.getVolume().compareTo(quote.getVolumeSize()) <= 0){
+        if(volume.getVolume().compareTo(quotes.get(quotes.size() - 1).getVolumeSize()) > 0) {
+            throw new FxConversionException("Amount greater than upper bound");
+        }
+
+        for (Quote quote : quotes) {
+            if (volume.getVolume().compareTo(quote.getVolumeSize()) <= 0) {
                 searchQuote = quote;
                 break;
             }
