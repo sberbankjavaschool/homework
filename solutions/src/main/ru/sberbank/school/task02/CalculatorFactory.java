ad209b4f90544c53c5561ff9656d294a6882ab86
Tanya Naumovets <naumovets.ts@gmail.com>
2019-05-02 20:07:55 +0300

    add @Override and checkstyle changes

--- solutions/src/main/ru/sberbank/school/task02/CalculatorFactory.java
+++ solutions/src/main/ru/sberbank/school/task02/CalculatorFactory.java
@@ -2,6 +2,7 @@ package ru.sberbank.school.task02;
 
 public class CalculatorFactory implements ServiceFactory {
 
+    @Override
     public FxConversionService getFxConversionService(ExternalQuotesService externalQuotesService) {
         return new Calculator(externalQuotesService);
     }
