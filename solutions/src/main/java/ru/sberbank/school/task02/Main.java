959ab99f3e037c0bcd7c9f97fab35d439831129d
Rinat Zahoda <mrsalatikus@gmail.com>
2019-05-04 02:38:03 +0300

    Решение через лямбду.

---
+++ solutions/src/main/java/ru/sberbank/school/task02/Main.java
@@ -0,0 +1,15 @@
+package ru.sberbank.school.task02;
+
+import ru.sberbank.school.task02.util.ClientOperation;
+import ru.sberbank.school.task02.util.ExternalQuotesServiceDemo;
+import ru.sberbank.school.task02.util.Symbol;
+
+import java.math.BigDecimal;
+
+public class Main {
+    public static void main(String[] args) {
+        ServiceFactory sf = new ServiceFactoryImpl();
+        Converter converter = (Converter) sf.getFxConversionService(new ExternalQuotesServiceDemo());
+        System.out.println(converter.convert(ClientOperation.BUY,Symbol.USD_RUB,new BigDecimal(100)));
+    }
+}
