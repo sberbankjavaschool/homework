d67a735987dcc6a4ddbeb82ce138c1806b35510c
IgnatMaloukhov <i.malo@outlook.com>
2019-05-04 14:25:18 +0400

    Try after youtube

---
+++ solutions/tasks/src/main/java/ru/sberbank/school/task02/FxConversionServiceImpl.java
@@ -0,0 +1,46 @@
+package ru.sberbank.school.task02;
+
+import ru.sberbank.school.task02.util.ClientOperation;
+import ru.sberbank.school.task02.util.ExternalQuotesServiceDemo;
+import ru.sberbank.school.task02.util.Quote;
+import ru.sberbank.school.task02.util.Symbol;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class FxConversionServiceImpl implements FxConversionService{
+
+    @Override
+    public BigDecimal convert(ClientOperation operation, Symbol symbol, BigDecimal amount) {
+
+        //Создание объекта со списком котировок
+        ExternalQuotesServiceDemo externalQuotesServiceDemo = new ExternalQuotesServiceDemo();
+        List<Quote> quotes = new ArrayList<>();
+        quotes = externalQuotesServiceDemo.getQuotes(symbol);
+
+        //Поиск первого элемента, значение которого больше amount
+        int k = 0;
+        while(quotes.get(k).getVolumeSize().compareTo(amount) < 0) {
+            k++;
+        }
+
+        //Индекс минимального элемента, который больше amount
+        int minIndex = k;
+        for(int i = k; i <  quotes.size(); i++) {
+
+            if(quotes.get(i).getVolumeSize().compareTo(amount) > 0)
+            {
+                if(quotes.get(i).getVolumeSize().compareTo(quotes.get(minIndex).getVolumeSize()) < 0) {
+                    minIndex = i;
+                }
+            }
+        }
+
+        return quotes.get(minIndex).getOffer();
+    }
+
+
+
+}
---
+++ solutions/tasks/src/main/java/ru/sberbank/school/task02/Main.java
@@ -0,0 +1,34 @@
+package ru.sberbank.school.task02;
+
+import ru.sberbank.school.task02.util.ClientOperation;
+import ru.sberbank.school.task02.util.ExternalQuotesServiceDemo;
+import ru.sberbank.school.task02.util.Symbol;
+
+import static ru.sberbank.school.task02.util.Symbol.USD_RUB;
+
+import java.math.BigDecimal;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        //Создание объекта со списком котировок
+        ExternalQuotesService externalQuotesService = new ExternalQuotesServiceDemo();
+
+        //Создание объекта валютного калькулятора FxConversionService с помощью ServiceFactory
+        ServiceFactory serviceFactory = new ServiceFactoryImpl();
+        FxConversionService fxConversionService = serviceFactory.getFxConversionService(externalQuotesService);
+
+        //Получение котировки
+        ClientOperation testClientOperation = ClientOperation.BUY;
+        Symbol testSymbol = USD_RUB;
+        BigDecimal testValue = BigDecimal.valueOf(1000);
+
+        BigDecimal testPrice = fxConversionService.convert(testClientOperation, testSymbol, testValue);
+        System.out.println(testPrice);
+
+
+    }
+
+}
