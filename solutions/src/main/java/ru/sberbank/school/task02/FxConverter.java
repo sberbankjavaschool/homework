91dc2ea48d1d2187e32073004f32f39deab8f652
vksychev <vksychev@yandex.ru>
2019-05-05 18:48:39 +0300

    Add FxConverter and FxConverterFactory

---
+++ solutions/src/main/java/ru/sberbank/school/task02/FxConverter.java
@@ -0,0 +1,56 @@
+package ru.sberbank.school.task02;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import ru.sberbank.school.task02.util.ClientOperation;
+import ru.sberbank.school.task02.util.Quote;
+import ru.sberbank.school.task02.util.Symbol;
+import ru.sberbank.school.task02.util.Volume;
+
+public class FxConverter implements FxConversionService {
+
+  private ExternalQuotesService Quotes;
+
+  FxConverter(ExternalQuotesService q) {
+    Quotes = q;
+  }
+
+  @Override
+  public BigDecimal convert(ClientOperation operation, Symbol symbol, BigDecimal amount) {
+    if (operation == null) {
+      throw new NullPointerException("Operation is null");
+    }
+
+    if (amount.compareTo(BigDecimal.ZERO) <= 0) {
+      throw new IllegalArgumentException("Amount less than 0");
+    }
+
+    BigDecimal curVolume = BigDecimal.ZERO;
+    Quote curQuote = Quotes.getQuotes(symbol).get(0);
+
+    for (Quote quote : Quotes.getQuotes(symbol)) {
+      BigDecimal volume = quote.getVolumeSize();
+      if (amount.compareTo(volume) < 0) {
+        if ((curVolume.compareTo(BigDecimal.ZERO) <= 0) || (volume.compareTo(curVolume) < 0)) {
+          curVolume = volume;
+          curQuote = quote;
+        } else if (volume.compareTo(curVolume) == 0) {
+          BigDecimal offer = quote.getOffer();
+          BigDecimal bid = quote.getBid();
+          if (operation == ClientOperation.BUY && offer.compareTo(curQuote.getOffer()) > 0) {
+            curQuote = quote;
+          } else if (operation == ClientOperation.SELL && bid.compareTo(curQuote.getBid()) < 0) {
+            curQuote = quote;
+          }
+        }
+      }
+    }
+    System.out.println(curVolume);
+    return (operation == ClientOperation.BUY) ? curQuote.getOffer() : curQuote.getBid();
+  }
+
+  private boolean isBuy(ClientOperation operation) {
+    return ClientOperation.BUY == operation;
+  }
+}
