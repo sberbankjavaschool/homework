b0f0b47d1d4b17a33711adfcf9101f9e0fdc8317
Vitaly Martyanov <vitalkamart@gmail.com>
2019-05-05 14:33:17 +0300

    (2) Martyanov

--- solutions/src/main/java/ru/sberbank/school/task02/Main.java
+++ solutions/src/main/java/ru/sberbank/school/task02/Main.java
@@ -39,10 +39,10 @@ public class Main {
                 + " expected 76 sell amount 100");
         System.out.println(fxConversionService.convert(
                 ClientOperation.BUY, Symbol.USD_RUB, BigDecimal.valueOf(9.000000044189101E8))
-                + " expected 85 buy amount 9.000000044189101E8");
+                + " expected 84 buy amount 9.000000044189101E8");
         System.out.println(fxConversionService.convert(
                 ClientOperation.SELL, Symbol.USD_RUB, BigDecimal.valueOf(9.000000044189101E8))
-                + " expected 75 sell amount 9.000000044189101E8");
+                + " expected 76 sell amount 9.000000044189101E8");
         System.out.println(
                 "10 compareTo 9: " + new BigDecimal(10).compareTo(new BigDecimal(9)));
     }
