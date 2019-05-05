78bb77dd4911a0e843be9a7816eb817d066f8e52
vksychev <vksychev@yandex.ru>
2019-05-05 18:48:39 +0300

    Add FxConverter and FxConverterFactory

---
+++ solutions/src/main/java/ru/sberbank/school/task02/FxConverterFactory.java
@@ -0,0 +1,20 @@
+package ru.sberbank.school.task02;
+
+import java.math.BigDecimal;
+
+import ru.sberbank.school.task02.util.ClientOperation;
+import ru.sberbank.school.task02.util.ExternalQuotesServiceDemo;
+import ru.sberbank.school.task02.util.Symbol;
+
+public class FxConverterFactory implements ServiceFactory {
+  @Override
+  public FxConversionService getFxConversionService(ExternalQuotesService externalQuotesService) {
+    return new FxConverter(externalQuotesService);
+  }
+
+  public static void main(String[] args) {
+    ServiceFactory factory = new FxConverterFactory();
+    FxConversionService converter = new FxConverter(new ExternalQuotesServiceDemo());
+    System.out.println(converter.convert(ClientOperation.BUY, Symbol.USD_RUB, new BigDecimal(99)));
+  }
+}
