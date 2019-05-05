f49074441fd1f23d380c1a43dc865f973a61545a
maxim <6tstiHLL6six>
2019-05-05 22:17:02 +0300

    implement calculator

---
+++ solutions/src/main/java/ServiceFactoryImpl.java
@@ -0,0 +1,11 @@
+import ru.sberbank.school.task02.ExternalQuotesService;
+import ru.sberbank.school.task02.FxConversionService;
+import ru.sberbank.school.task02.ServiceFactory;
+
+public class ServiceFactoryImpl implements ServiceFactory {
+
+    @Override
+    public FxConversionService getFxConversionService(ExternalQuotesService externalQuotesService) {
+        return new SimpleFxConversionService(externalQuotesService);
+    }
+}
---
+++ solutions/src/main/java/SimpleFxConversionService.java
@@ -0,0 +1,65 @@
+import ru.sberbank.school.task02.ExternalQuotesService;
+import ru.sberbank.school.task02.FxConversionService;
+import ru.sberbank.school.task02.util.ClientOperation;
+import ru.sberbank.school.task02.util.Quote;
+import ru.sberbank.school.task02.util.Symbol;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import static ru.sberbank.school.task02.util.ClientOperation.BUY;
+
+public class SimpleFxConversionService implements FxConversionService {
+
+    private ExternalQuotesService externalQuotesService;
+
+    public SimpleFxConversionService(ExternalQuotesService externalQuotesService) {
+        this.externalQuotesService = externalQuotesService;
+    }
+
+    @Override
+    public BigDecimal convert(ClientOperation operation, Symbol symbol, BigDecimal amount) {
+        if (operation == null) {
+            throw new IllegalArgumentException("operation is not defined");
+        }
+
+        if (symbol == null) {
+            throw new IllegalArgumentException("instrument is not defined");
+        }
+
+        if (amount == null || (amount.compareTo(BigDecimal.ZERO) <= 0) ) {
+            throw new IllegalArgumentException("amount is not defined or not positive");
+        }
+
+        List<Quote> quotes = externalQuotesService.getQuotes(symbol);
+        Quote quote = null;
+        for (Quote quoteCandidate: quotes) {
+            if (quoteCandidate == null) {
+                continue;
+            }
+
+            if (quoteCandidate.isInfinity()) {
+                quote = quoteCandidate;
+            }
+
+            if (isQuoteVolumeBigger(quoteCandidate, amount)) {
+                if (quote == null) {
+                    quote = quoteCandidate;
+                }
+                else {
+                    quote = isQuoteVolumeBigger(quoteCandidate, quote.getVolumeSize()) ? quoteCandidate : quote;
+                }
+            }
+        }
+
+        if (quote == null) {
+            throw new IllegalStateException("No acceptable quotes were found for given");
+        }
+
+        return operation == BUY ? quote.getOffer() : quote.getBid();
+    }
+
+    private boolean isQuoteVolumeBigger(Quote quote, BigDecimal amount) {
+        return quote.getVolumeSize().compareTo(amount) > 0;
+    }
+}
