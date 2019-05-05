0bd6daf9206c90aaed6a18b8c5c09598452e58f1
Vitaly Martyanov <vitalkamart@gmail.com>
2019-05-05 10:27:20 +0300

    (2) Martyanov

--- solutions/src/main/java/ru/sberbank/school/task02/Main.java
+++ solutions/src/main/java/ru/sberbank/school/task02/Main.java
@@ -16,24 +16,32 @@ public class Main {
         ServiceFactory serviceFactory = new ServiceFactoryImpl();
         ExternalQuotesService externalQuotesService = new ExternalQuotesServiceDemo();
         for (Quote q : externalQuotesService.getQuotes(Symbol.USD_RUB)) {
-            System.out.println("offer: " + q.getOffer() +  " bid " + q.getBid()+"  Volume: " + q.getVolumeSize());
+            System.out.println("offer: " + q.getOffer() +  " bid " + q.getBid()
+                    + "  Volume: " + q.getVolumeSize());
         }
         System.out.println("========================================================");
 
         FxConversionService fxConversionService = serviceFactory.getFxConversionService(externalQuotesService);
         System.out.println(fxConversionService.convert(
-                ClientOperation.BUY, Symbol.USD_RUB, BigDecimal.valueOf(600)) + " expected 83 buy amount 600");
+                ClientOperation.BUY, Symbol.USD_RUB, BigDecimal.valueOf(600))
+                + " expected 83 buy amount 600");
         System.out.println(fxConversionService.convert(
-                ClientOperation.BUY, Symbol.USD_RUB, BigDecimal.valueOf(5_000)) + " expected 82 buy amount 5000");
+                ClientOperation.BUY, Symbol.USD_RUB, BigDecimal.valueOf(5_000))
+                + " expected 82 buy amount 5000");
         System.out.println(fxConversionService.convert(
-                ClientOperation.BUY, Symbol.USD_RUB, BigDecimal.valueOf(300_000)) + " expected 83 buy amount 300 000");
+                ClientOperation.BUY, Symbol.USD_RUB, BigDecimal.valueOf(300_000))
+                + " expected 83 buy amount 300 000");
         System.out.println(fxConversionService.convert(
-                ClientOperation.BUY, Symbol.USD_RUB, BigDecimal.valueOf(100)) + " expected 85 buy amount 100");
+                ClientOperation.BUY, Symbol.USD_RUB, BigDecimal.valueOf(100))
+                + " expected 85 buy amount 100");
         System.out.println(fxConversionService.convert(
-                ClientOperation.SELL, Symbol.USD_RUB, BigDecimal.valueOf(100)) + " expected 75 sell amount 100");
+                ClientOperation.SELL, Symbol.USD_RUB, BigDecimal.valueOf(100))
+                + " expected 75 sell amount 100");
         System.out.println(fxConversionService.convert(
-                ClientOperation.BUY, Symbol.USD_RUB, BigDecimal.valueOf(9.000000044189101E8)) + " expected 85 buy amount 9.000000044189101E8");
+                ClientOperation.BUY, Symbol.USD_RUB, BigDecimal.valueOf(9.000000044189101E8))
+                + " expected 85 buy amount 9.000000044189101E8");
         System.out.println(fxConversionService.convert(
-                ClientOperation.SELL, Symbol.USD_RUB, BigDecimal.valueOf(9.000000044189101E8)) + " expected 75 sell amount 9.000000044189101E8");
+                ClientOperation.SELL, Symbol.USD_RUB, BigDecimal.valueOf(9.000000044189101E8))
+                + " expected 75 sell amount 9.000000044189101E8");
     }
 }
