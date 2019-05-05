436e17c2635cd923022be2b06406a619aa12485c
vksychev <vksychev@yandex.ru>
2019-05-05 22:07:09 +0300

    Add infinity support

--- solutions/src/main/java/ru/sberbank/school/task02/FxConverterFactory.java
+++ solutions/src/main/java/ru/sberbank/school/task02/FxConverterFactory.java
@@ -7,13 +7,13 @@ import ru.sberbank.school.task02.util.ExternalQuotesServiceDemo;
 import ru.sberbank.school.task02.util.Symbol;
 
 public class FxConverterFactory implements ServiceFactory {
-  @Override
-  public FxConversionService getFxConversionService(ExternalQuotesService externalQuotesService) {
-    return new FxConverter(externalQuotesService);
-  }
+    @Override
+    public FxConversionService getFxConversionService(ExternalQuotesService externalQuotesService) {
+        return new FxConverter(externalQuotesService);
+    }
 
-  public static void main(String[] args) {
-    FxConversionService converter = new FxConverter(new ExternalQuotesServiceDemo());
-    System.out.println(converter.convert(ClientOperation.BUY, Symbol.USD_RUB, new BigDecimal(1000000)));
-  }
+    public static void main(String[] args) {
+        FxConversionService converter = new FxConverter(new ExternalQuotesServiceDemo());
+        System.out.println(converter.convert(ClientOperation.BUY, Symbol.USD_RUB, new BigDecimal(100000)));
+    }
 }
