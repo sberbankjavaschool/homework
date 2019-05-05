3814aceeae27f0204e402dc5c2bbd19135256bde
brddlrr <mishamavrin@gmail.com>
2019-05-06 01:02:07 +0300

    qoute picking algorithm changed

--- solutions/src/main/java/ru/sberbank/school/task02/CurrencyCalc.java
+++ solutions/src/main/java/ru/sberbank/school/task02/CurrencyCalc.java
@@ -18,21 +18,21 @@ public class CurrencyCalc implements FxConversionService {
 
     @Override
     public BigDecimal convert(ClientOperation operation, Symbol symbol, BigDecimal amount) {
-        if(operation == null){
+        if (operation == null) {
             throw new NullPointerException("operation missing");
         }
-        if(amount == null){
+        if (amount == null) {
             throw new NullPointerException("amount missing");
         }
-        if(symbol == null ){
+        if (symbol == null) {
             throw new NullPointerException("symbol missing");
         }
-        if(amount.compareTo(BigDecimal.ZERO) <= 0) {
+        if (amount.compareTo(BigDecimal.ZERO) <= 0) {
             throw new IllegalArgumentException("amount should be positive");
         }
 
         List<Quote> quotesList = quotes.getQuotes(symbol);
-        if(quotesList.isEmpty()) {
+        if (quotesList.isEmpty()) {
             throw new FxConversionException("quotes unavailable");
         }
 
@@ -44,26 +44,20 @@ public class CurrencyCalc implements FxConversionService {
     private Quote pickQuote(List<Quote> quoteList, BigDecimal amount) {
         Quote pickedQuote = null;
 
-        for(Quote possibleQuote : quoteList){
-            if(compareVolumes(possibleQuote.getVolumeSize(), amount) > 0) {
-                if(pickedQuote == null ||
-                        compareVolumes(pickedQuote.getVolumeSize(), possibleQuote.getVolumeSize())  < 0 ) {
-                    pickedQuote = possibleQuote;
+        for (Quote tempQuote : quoteList) {
+            if (pickedQuote == null && tempQuote.isInfinity()) {
+                pickedQuote = tempQuote;
+            }
+
+            if (tempQuote.getVolumeSize().compareTo(amount) >= 0) {
+                if (pickedQuote == null
+                        || pickedQuote.isInfinity()
+                        || tempQuote.getVolumeSize().compareTo(pickedQuote.getVolumeSize()) <= 0) {
+                    pickedQuote = tempQuote;
                 }
             }
         }
 
         return pickedQuote == null ? quoteList.get(0) : pickedQuote;
-    }
-
-    private int compareVolumes(BigDecimal v1, BigDecimal v2) {
-        if(v1.compareTo(BigDecimal.ZERO) < 0) {
-            return 1;
-        }
-        if(v2.compareTo(BigDecimal.ZERO) < 0) {
-            return -1;
-        }
-
-        return v1.compareTo(v2);
     }
 }
