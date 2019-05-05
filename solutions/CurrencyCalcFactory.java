87a690ac6cdaf761b4299edc11784edf4242bf3e
brddlrr <mishamavrin@gmail.com>
2019-05-05 16:44:53 +0300

    Calculator and service factory added

---
+++ solutions/CurrencyCalcFactory.java
@@ -0,0 +1,10 @@
+package ru.sberbank.school.task02;
+
+public class CurrencyCalcFactory implements ServiceFactory {
+
+
+    @Override
+    public FxConversionService getFxConversionService(ExternalQuotesService externalQuotesService) {
+        return new CurrencyCalc(externalQuotesService);
+    }
+}
