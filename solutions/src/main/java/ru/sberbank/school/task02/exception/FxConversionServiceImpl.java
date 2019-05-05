2f85b75af952c3dc8ded37a0bcee0095cced9dca
Ganzalo <Ganzalo2010@gmail.com>
2019-05-05 20:29:01 +0300

    codestyle fix

--- solutions/src/main/java/ru/sberbank/school/task02/exception/FxConversionServiceImpl.java
+++ solutions/src/main/java/ru/sberbank/school/task02/exception/FxConversionServiceImpl.java
@@ -11,46 +11,48 @@ import java.math.BigDecimal;
 import java.util.List;
 
 public class FxConversionServiceImpl implements FxConversionService {
-  private ExternalQuotesService externalQuotesService;
+    private ExternalQuotesService externalQuotesService;
 
-  private ClientOperation operation;
+    private ClientOperation operation;
 
-  public FxConversionServiceImpl(ExternalQuotesService externalQuotesService) {
-    this.externalQuotesService = externalQuotesService;
-  }
-
-  @Override
-  public BigDecimal convert(@NonNull ClientOperation operation, @NonNull Symbol symbol,
-                            @NonNull BigDecimal amount) {
-    this.operation = operation;
-    if (checkCorrectConvert(amount)) {
-      throw new ConverterConfigurationException("Amount меньше или равно нулю");
-    }
-    List<Quote> quotes = externalQuotesService.getQuotes(symbol);
-    if (quotes.isEmpty()) {
-      throw new ConverterConfigurationException("externalQuotesService пустой");
+    public FxConversionServiceImpl(ExternalQuotesService externalQuotesService) {
+        this.externalQuotesService = externalQuotesService;
     }
 
-    Quote bestQuotes = quotes.get(0);
-    for (Quote quote : quotes) {
-      if (amount.compareTo(quote.getVolumeSize()) < 0 || quote.isInfinity()) {
-        if (operationPrice(bestQuotes).compareTo(operationPrice(quote)) > 0) {
-          bestQuotes = quote;
+    @Override
+    public BigDecimal convert(@NonNull ClientOperation operation, @NonNull Symbol symbol,
+                              @NonNull BigDecimal amount) {
+        this.operation = operation;
+        if (checkCorrectConvert(amount)) {
+            throw new ConverterConfigurationException("Amount меньше или равно нулю");
         }
-      }
-    }
-    return operationPrice(bestQuotes);
-  }
 
-  private boolean checkCorrectConvert(BigDecimal amount) {
-    boolean state = false;
-    if (amount.compareTo(BigDecimal.ZERO) > 0) {
-      state = true;
-    }
-    return  state;
-  }
+        List<Quote> quotes = externalQuotesService.getQuotes(symbol);
+        if (quotes.isEmpty()) {
+            throw new ConverterConfigurationException("externalQuotesService пустой");
+        }
 
-  private BigDecimal operationPrice(Quote quote) {
-    return this.operation == ClientOperation.BUY ? quote.getOffer() : quote.getBid();
-  }
+        Quote bestQuotes = quotes.get(0);
+        for (Quote quote : quotes) {
+            if (amount.compareTo(quote.getVolumeSize()) < 0 || quote.isInfinity()) {
+                if (operationPrice(bestQuotes).compareTo(operationPrice(quote)) > 0) {
+                    bestQuotes = quote;
+                }
+            }
+        }
+
+        return operationPrice(bestQuotes);
+    }
+
+    private boolean checkCorrectConvert(BigDecimal amount) {
+        boolean state = false;
+        if (amount.compareTo(BigDecimal.ZERO) > 0) {
+            state = true;
+        }
+        return  state;
+    }
+
+    private BigDecimal operationPrice(Quote quote) {
+        return this.operation == ClientOperation.BUY ? quote.getOffer() : quote.getBid();
+    }
 }
