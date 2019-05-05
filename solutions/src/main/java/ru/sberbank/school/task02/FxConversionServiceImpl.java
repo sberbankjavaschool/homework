07fe7f31108a942d0e76935e844aefd4d7070ffe
Vladimir Nastoyaschiy <maga_net@mail.ru>
2019-05-05 07:53:21 +0300

    убрал лишний проход по котировкам

--- solutions/src/main/java/ru/sberbank/school/task02/FxConversionServiceImpl.java
+++ solutions/src/main/java/ru/sberbank/school/task02/FxConversionServiceImpl.java
@@ -1,6 +1,7 @@
 package ru.sberbank.school.task02;
 
 
+import ru.sberbank.school.task02.exception.FxConversionException;
 import ru.sberbank.school.task02.util.ClientOperation;
 import ru.sberbank.school.task02.util.Quote;
 import ru.sberbank.school.task02.util.Symbol;
@@ -18,21 +19,20 @@ public class FxConversionServiceImpl implements FxConversionService {
     @Override
     public BigDecimal convert(ClientOperation operation, Symbol symbol, BigDecimal amount) {
         List<Quote> quoteList = externalQuotesService.getQuotes(symbol);
-        Quote targetQuote = quoteList.get(0);
+        Quote targetQuote = null;
         for (Quote currentQuote : quoteList) {
-            if (currentQuote.isInfinity()) {
+            if (targetQuote == null && currentQuote.isInfinity()) {
                 targetQuote = currentQuote;
-                break;
-            }
-        }
-        for (Quote currentQuote : quoteList) {
-            if (currentQuote.getVolumeSize().compareTo(amount) > 0) {
-                if (targetQuote.isInfinity()) {
+            } else if (currentQuote.getVolumeSize().compareTo(amount) > 0) {
+                if (targetQuote == null || targetQuote.isInfinity()) {
                     targetQuote = currentQuote;
                 } else if (targetQuote.getVolumeSize().compareTo(currentQuote.getVolumeSize()) > 0) {
                     targetQuote = currentQuote;
                 }
             }
+        }
+        if (targetQuote == null) {
+            throw new FxConversionException("Нет подходящего диапазона котировок");
         }
         return ClientOperation.BUY == operation ? targetQuote.getOffer() : targetQuote.getBid();
     }
