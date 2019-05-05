c881eb2a988dde8a7d03a2c9fd11cf37721de934
IgnatMaloukhov <i.malo@outlook.com>
2019-05-05 10:58:33 +0400

    Список котировок Quote сделан локальной переменной метода convert

--- solutions/src/main/java/ru/sberbank/school/task02/ServiceFactoryImpl.java
+++ solutions/src/main/java/ru/sberbank/school/task02/ServiceFactoryImpl.java
@@ -9,7 +9,7 @@ public class ServiceFactoryImpl implements ServiceFactory {
     @Override
     public FxConversionService getFxConversionService(ExternalQuotesService externalQuotesService) throws FxConversionException {
 
-        if(externalQuotesService == null) {
+        if (externalQuotesService == null) {
             throw new FxConversionException("Неправильная ссылка на объект котировок ExternalQuotesService");
         }
         else {
