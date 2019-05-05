920f046b8db453523549035349eb7ce4c3510b52
anaroman <romacia2007@mail.ru>
2019-05-05 15:10:46 +0300

    task02 sntx fix

--- solutions/src/main/java/ru/sberbank/school/task02/Test.java
+++ solutions/src/main/java/ru/sberbank/school/task02/Test.java
@@ -1,4 +1,5 @@
 package ru.sberbank.school.task02;
+
 import ru.sberbank.school.task02.util.ClientOperation;
 import ru.sberbank.school.task02.util.Symbol;
 
@@ -9,7 +10,6 @@ public class Test {
 
         ServiceFactory factory = new ServiceFactoryC();
         ExternalQuotesService quotesService = new ExternalQuotesServiceC();
-
         FxConversionService exCalc = factory.getFxConversionService(quotesService);
         BigDecimal offer = exCalc.convert(ClientOperation.BUY, Symbol.USD_RUB, BigDecimal.valueOf(104.7559849936236));
         System.out.println(offer);
