4a6e390e2a736cd304fe161ca137dd8862db75fb
Alex K <bormoshka@gmail.com>
2019-05-07 18:25:07 +0300

    Task 03

--- tasks/src/main/java/ru/sberbank/school/SchoolTask.java
+++ tasks/src/main/java/ru/sberbank/school/SchoolTask.java
@@ -8,7 +8,8 @@ import lombok.Getter;
 public enum SchoolTask {
     FX_CALC_02_01("Вал. Калькулятор ч.1", 2),
     FX_CALC_02_02("Вал. Калькулятор ч.2", 3),
-    FX_CALC_02_03("Вал. Калькулятор ч.3", 4);
+    FX_CALC_02_03("Вал. Калькулятор ч.3", 4),
+    FX_CALC_03_01("Форматтер", 5);
 
     private final String name;
     private final int code;
