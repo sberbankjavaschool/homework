4b530a18a781e479d55820fa45082586d7bebfd8
Alex K <bormoshka@gmail.com>
2019-05-07 20:42:22 +0300

    Squashed commit of the following:
    commit 0a345547b2f33cc33447621404e349af4ef08026
    Author: Alex K <bormoshka@gmail.com>
    Date:   Fri May 3 13:17:53 2019 +0300
    
        Добавил описание методам.
    
    commit 342f0a8bf04bf424b48118165e43416eec4f6204
    Merge: eae6391 b39b35d
    Author: Alex K <bormoshka@gmail.com>
    Date:   Thu May 2 23:10:53 2019 +0300
    
        Merge branches 'Melnikov' and 'source' of https://github.com/sberbankjavaschool/homework into Melnikov
    
    commit eae6391071d61101b1bf44824b312fd14e030d33
    Merge: d5c104e f47814c
    Author: Alexey Kolmogorov <36761659+kolmogorov-aa@users.noreply.github.com>
    Date:   Wed May 1 17:46:33 2019 +0300
    
        Merge pull request #23 from Pelynthe/Melnikov
    
        (2) Мельников
    
    commit f47814c770110bb92fd3d411daac981f2ea2a4ee
    Author: Gregory <GregoryMelnikov@gmail.com>
    Date:   Tue Apr 30 23:19:05 2019 +0300
    
        task02: some hook 2
    
    commit a18dcb75ae506a714eacf0f6394266c7d07f1140
    Author: Gregory <GregoryMelnikov@gmail.com>
    Date:   Tue Apr 30 22:58:16 2019 +0300
    
        task02: some hook
    
    commit 753ac078fca1d356db08ae89daee0ffe980bbac3
    Merge: 334d444 5ae7892
    Author: Gregory <GregoryMelnikov@gmail.com>
    Date:   Tue Apr 30 22:29:47 2019 +0300
    
        Merge remote-tracking branch 'origin/Melnikov' into Melnikov
    
        # Conflicts:
        #	solutions/src/main/java/ru/sberbank/school/task02/services/implementation/FxConversionServiceImpl.java
        #	solutions/src/main/java/ru/sberbank/school/task02/services/implementation/ServiceFactoryImpl.java
    
    commit 334d44437908d9539f784dbe8c726e8df0791d7b
    Author: Gregory <GregoryMelnikov@gmail.com>
    Date:   Tue Apr 30 22:23:02 2019 +0300
    
        task02: some fixes
    
    commit db8c36078684717eddeb4b577c431ddda77f3528
    Author: Gregory <GregoryMelnikov@gmail.com>
    Date:   Sun Apr 28 23:58:59 2019 +0300
    
        task02: new repo
    
    commit 4676763706385a533dc086c9f626a76162fa8a27
    Author: Gregory <GregoryMelnikov@gmail.com>
    Date:   Sun Apr 28 22:59:15 2019 +0300
    
        task02: new repo
    
    commit 511e95766bb1727b3da1ccd50858f942c144f441
    Author: Alex K <bormoshka@gmail.com>
    Date:   Mon Apr 29 23:27:57 2019 +0300
    
        Добавлен lombok в модуль solutions
    
    commit d9d4dd0242ba4797f7d2d257b90d05caa6d39b6e
    Author: Alex K <bormoshka@gmail.com>
    Date:   Mon Apr 29 12:13:02 2019 +0300
    
        Обновлены задания. черновой вариант
    
    commit 5ae7892f4d0fc8c69040fe66b93308818e42272a
    Author: Gregory <GregoryMelnikov@gmail.com>
    Date:   Sun Apr 28 23:58:59 2019 +0300
    
        task02: new repo
    
    commit e311ce02aa9a21c808c7de72d339fb275381df98
    Author: Gregory <GregoryMelnikov@gmail.com>
    Date:   Sun Apr 28 22:59:15 2019 +0300
    
        task02: new repo

---
+++ solutions/src/main/java/ru/sberbank/school/task02/services/implementation/InternalQuotesServiceImpl.java
@@ -0,0 +1,49 @@
+package ru.sberbank.school.task02.services.implementation;
+
+import lombok.RequiredArgsConstructor;
+import ru.sberbank.school.task02.ExternalQuotesService;
+import ru.sberbank.school.task02.services.InternalQuotesService;
+import ru.sberbank.school.task02.exeption.EmptyQuoteList;
+import ru.sberbank.school.task02.util.Quote;
+import ru.sberbank.school.task02.util.Symbol;
+
+import java.util.List;
+
+/**
+ * Реализация внутреннего сервиса котировок.
+ * Created by Gregory Melnikov at 28.04.2019
+ */
+@RequiredArgsConstructor
+public class InternalQuotesServiceImpl implements InternalQuotesService {
+
+    private final ExternalQuotesService externalQuotesService;
+
+    /**
+     * 28.04.2019
+     * Получает список котировок от внешнего сервиса, выполняет проверку на наличие элементов в списке и сортировку
+     *
+     * @param symbol Инструмент
+     * @return отсортированный список котировок
+     */
+    @Override
+    public List<Quote> getQuotes(Symbol symbol) {
+        List<Quote> quotes = externalQuotesService.getQuotes(symbol);
+
+        if (quotes.isEmpty()) {
+            throw new EmptyQuoteList("External quotes service must return at least one quote!");
+        }
+
+        quotes.sort((previos, current) -> {
+            int result = previos.getVolumeSize().compareTo(current.getVolumeSize());
+            //Дополнительная сортировка при одинаковом значении объема. Сбивает тесты
+//            if (result == 0) {
+//                result = previos.getBid().compareTo(current.getBid());
+//            }
+            if (result == 0) {
+                result = current.getBid().compareTo(previos.getBid());
+            }
+            return result;
+        });
+        return quotes;
+    }
+}
