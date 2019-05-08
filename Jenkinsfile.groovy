aa12737b912422c344e219f1e2431b4cad96a071
Alex K <bormoshka@gmail.com>
2019-05-08 13:01:56 +0300

    Отключил авторебейз

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -27,15 +27,15 @@ pipeline {
                         removeLabel('WRONG BRANCH')
                     }
                     try {
-                        try {
-                        sh "./gradlew --stacktrace tryToPushToForkRepo " +
-                                "-PsourceBranch='${pullRequest.headRef}' " +
-                                "-PforkRepo='https://github.com/${CHANGE_AUTHOR}/homework.git'"
-                        } catch (err) {
-                            pullRequest.comment("Не смог автоматически запушить в твой форк."
-                                    + " Добавь jenkins-java-school-2019 в коллабораторы и скинь ссылку в общий чат.\n" +
-                                    "Если это уже было сделано, значит автоматический пуш невозможен. Делай руками.")
-                        }
+                        //try {
+                        //    sh "./gradlew --stacktrace tryToPushToForkRepo " +
+                        //            "-PsourceBranch='${pullRequest.headRef}' " +
+                        //            "-PforkRepo='https://github.com/${CHANGE_AUTHOR}/homework.git'"
+                        //} catch (err) {
+                        //    pullRequest.comment("Не смог автоматически запушить в твой форк."
+                        //            + " Добавь jenkins-java-school-2019 в коллабораторы и скинь ссылку в общий чат.\n" +
+                        //            "Если это уже было сделано, значит автоматический пуш невозможен. Делай руками.")
+                        //}
                         sh "./gradlew --stacktrace checkIfSourceBranchPulled " +
                                 "-PsourceBranch='${pullRequest.headRef}' " +
                                 "-PforkRepo='https://github.com/${CHANGE_AUTHOR}/homework.git'"
