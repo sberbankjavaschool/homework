2ed402c505502b124f195b143f8a1093f90e09da
anaroman <romacia2007@mail.ru>
2019-05-09 21:22:25 +0300

    task 02 id3 one more PR

--- solutions/src/main/java/ru/sberbank/school/task02/FxConversionServiceImpl.java
+++ solutions/src/main/java/ru/sberbank/school/task02/FxConversionServiceImpl.java
@@ -75,8 +75,8 @@ public class FxConversionServiceImpl implements ExtendedFxConversionService {
 
         BigDecimal rightAmount = amount.divide(rightCur, 10, RoundingMode.HALF_UP);
 
-        BigDecimal revCur = operation == ClientOperation.BUY ? convert(ClientOperation.SELL, symbol, amount) :
-                convert(ClientOperation.BUY, symbol, amount);
+        BigDecimal revCur = operation == ClientOperation.BUY ? convert(ClientOperation.SELL, symbol, rightAmount) :
+                convert(ClientOperation.BUY, symbol, rightAmount);
 
         BigDecimal price = BigDecimal.ONE.divide(revCur, 10, RoundingMode.HALF_UP);
         return Optional.ofNullable(price);
