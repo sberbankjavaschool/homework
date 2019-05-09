a443452fec181a65db9b7560b2e5d19a4cb660bd
Tanya Naumovets <naumovets.ts@gmail.com>
2019-05-09 19:46:24 +0300

    Add ExtendedCalculator and test

---
+++ solutions/src/main/java/ru/sberbank/school/task02/ExtendedCalculator.java
@@ -0,0 +1,83 @@
+package ru.sberbank.school.task02;
+
+import java.math.BigDecimal;
+import java.math.MathContext;
+import java.util.*;
+
+import ru.sberbank.school.task02.util.*;
+
+/**
+ * Реализация обратного валютного калькулятора.
+ */
+public class ExtendedCalculator extends Calculator implements ExtendedFxConversionService {
+
+    public ExtendedCalculator(ExternalQuotesService externalQuotesService) {
+        super(externalQuotesService);
+    }
+
+    @Override
+    public Optional<BigDecimal> convertReversed(ClientOperation operation, Symbol symbol, BigDecimal amount,
+                                                Beneficiary beneficiary) {
+
+        List<Quote> quotes = externalQuotesService.getQuotes(symbol);
+        if (quotes == null || quotes.isEmpty()) {
+            throw new EmptyQuoteException("No quotes!");
+        }
+
+        List<ReverseQuote> reverseQuotes = getReverseQuotes(quotes, operation);
+        List<BigDecimal> prices = new ArrayList<>();
+
+        for (ReverseQuote r : reverseQuotes) {
+            if (r.getVolumeFrom().isInfinity() && amount.compareTo(r.getVolumeToSize()) < 0
+                    || amount.compareTo(r.getVolumeFromSize()) >= 0 && amount.compareTo(r.getVolumeToSize()) < 0) {
+                prices.add(r.getPrice());
+            }
+        }
+
+        if (prices.isEmpty()) {
+            return Optional.empty();
+        }
+
+        Collections.sort(prices);
+        return Optional.of(beneficiary == Beneficiary.CLIENT ? prices.get(prices.size() - 1) : prices.get(0));
+    }
+
+    @Override
+    public Optional<BigDecimal> convertReversed(ClientOperation operation, Symbol symbol, BigDecimal amount,
+                                                double delta, Beneficiary beneficiary) {
+        return Optional.empty();
+    }
+
+    private void sortQuotes(List<Quote> quotes) {
+        quotes.sort( (q1, q2) -> {
+            if (q1.isInfinity()) {
+                return 1;
+            }
+            if (q2.isInfinity()) {
+                return -1;
+            }
+            return q1.getVolumeSize().compareTo(q2.getVolumeSize());
+        });
+    }
+
+    public List<ReverseQuote> getReverseQuotes(List<Quote> quotes, ClientOperation operation) {
+
+        sortQuotes(quotes);
+        List<ReverseQuote> reverseQuotes = new ArrayList<>();
+
+        for (int i = 0; i < quotes.size(); i++) {
+            BigDecimal price = operation == ClientOperation.SELL
+                    ? quotes.get(i).getBid() : quotes.get(i).getOffer();
+            Volume volumeFrom = i == 0
+                    ? Volume.from(0) : Volume.from(quotes.get(i - 1).getVolumeSize().multiply(price));
+            Volume volumeTo = Volume.from(quotes.get(i).getVolumeSize().multiply(price));
+            price = BigDecimal.ONE.divide(price, MathContext.DECIMAL32);
+            ReverseQuote r = new ReverseQuote(volumeFrom, volumeTo, price);
+            reverseQuotes.add(r);
+        }
+
+        return reverseQuotes;
+    }
+
+}
+
--- solutions/src/main/java/ru/sberbank/school/task02/Solution.java
+++ solutions/src/main/java/ru/sberbank/school/task02/Solution.java
@@ -1,8 +1,10 @@
 package ru.sberbank.school.task02;
 
 import java.math.BigDecimal;
+import java.util.Optional;
 
 import ru.sberbank.school.task02.exception.FxConversionException;
+import ru.sberbank.school.task02.util.Beneficiary;
 import ru.sberbank.school.task02.util.ClientOperation;
 import ru.sberbank.school.task02.util.ExternalQuotesServiceDemo;
 import ru.sberbank.school.task02.util.Symbol;
@@ -11,17 +13,33 @@ public class Solution {
     public static void main(String[] args) {
 
         CalculatorFactory calculatorFactory = new CalculatorFactory();
-        // получаем наш валютный калькулятор
+
         FxConversionService calculator =
                 calculatorFactory.getFxConversionService(new ExternalQuotesServiceDemo());
 
+        ExtendedFxConversionService extendedCalculator =
+                calculatorFactory.getExtendedFxConversionService(new ExternalQuotesServiceDemo());
+
+        Beneficiary beneficiary = System.getenv("Beneficiary") == "CLIENT"
+                ? Beneficiary.CLIENT : Beneficiary.BANK;
+
         try {
-            BigDecimal price = calculator.convert(ClientOperation.BUY,
-                    Symbol.USD_RUB, BigDecimal.valueOf(11_000_000));
-            System.out.println(price);
-        } catch (FxConversionException ex) {
-            System.out.println(ex.getMessage());
+            BigDecimal priceUsd = calculator.convert(ClientOperation.SELL,
+                    Symbol.USD_RUB, new BigDecimal(10_000_000));
+            System.out.println("USD: " + priceUsd);
+
+            Optional<BigDecimal> priceRub = extendedCalculator.convertReversed(ClientOperation.BUY, Symbol.USD_RUB,
+                    new BigDecimal(83963.2994), beneficiary);
+            if (priceRub.isPresent()) {
+                System.out.println("RUB " + priceRub.get());
+            } else {
+                System.out.println("No reverseQuotes!");
+            }
         } catch (NullPointerException ex) {
+            System.out.println(ex.getMessage());
+        } catch (IllegalArgumentException ex) {
+            System.out.println(ex.getMessage());
+        } catch (FxConversionException ex) {
             System.out.println(ex.getMessage());
         }
     }
