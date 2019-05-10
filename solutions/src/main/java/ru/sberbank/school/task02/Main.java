895bf97dab0376d75a8e8bb09282bafd5c2e0456
Alex K <bormoshka@gmail.com>
2019-05-11 01:14:58 +0300

    Задание id4.

--- solutions/src/main/java/ru/sberbank/school/task02/Main.java
+++ solutions/src/main/java/ru/sberbank/school/task02/Main.java
@@ -1,15 +1,25 @@
 package ru.sberbank.school.task02;
 
-import ru.sberbank.school.task02.util.ClientOperation;
-import ru.sberbank.school.task02.util.ExternalQuotesServiceDemo;
-import ru.sberbank.school.task02.util.Symbol;
-
-import java.math.BigDecimal;
+import ru.sberbank.school.task02.exception.ConverterConfigurationException;
+import ru.sberbank.school.task02.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        ServiceFactory sf = new ServiceFactoryImpl();
-        Converter converter = (Converter) sf.getFxConversionService(new ExternalQuotesServiceDemo());
-        System.out.println(converter.convert(ClientOperation.SELL,Symbol.USD_RUB,new BigDecimal(9.0E8)));
+
+        if (args.length < 3) {
+            throw new ConverterConfigurationException("Вы ввели мало параметров!");
+        }
+
+        ExternalQuotesService externalQuotesService = new ExternalQuotesServiceDemo();
+        ClientController client = new ClientController(externalQuotesService);
+        String symbol = args[0];
+        String operation = args[1];
+        String amount = args[2];
+
+        FxRequest request = new FxRequest(symbol, operation, amount);
+
+        FxResponse response = client.fetchResult(request);
+
+        System.out.println(response);
     }
 }
