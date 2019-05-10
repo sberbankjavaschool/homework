aeb186b033f9df8b28c8a07f4565831eca023308
Alex K <bormoshka@gmail.com>
2019-05-11 01:12:36 +0300

    Первое задание/ Сквош

---
+++ solutions/src/main/java/ru/sberbank/school/task02/Converter.java
@@ -0,0 +1,63 @@
+package ru.sberbank.school.task02;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import lombok.NonNull;
+import ru.sberbank.school.task02.exception.FxConversionException;
+import ru.sberbank.school.task02.util.ClientOperation;
+import ru.sberbank.school.task02.util.Quote;
+import ru.sberbank.school.task02.util.Symbol;
+
+public class Converter implements FxConversionService {
+    private ExternalQuotesService externalQuotesService;
+
+    public Converter(ExternalQuotesService externalQuotesService) {
+        this.externalQuotesService = externalQuotesService;
+    }
+
+
+    @Override
+    public BigDecimal convert(@NonNull ClientOperation operation, @NonNull Symbol symbol, @NonNull BigDecimal amount) {
+
+        if (amount.signum() <= 0) {
+            throw new IllegalArgumentException("Введите корректное значение объема!");
+        }
+
+        List<Quote> quotes = externalQuotesService.getQuotes(symbol);
+
+        if (quotes.isEmpty()) {
+            throw new FxConversionException("Список валют пуст!");
+        }
+
+        Quote result = null;
+
+        for (Quote quote : quotes) {
+            if (amount.compareTo(quote.getVolumeSize()) < 0 || quote.isInfinity()) {
+                if (result == null || quoteComparison(quote,result) < 0) {
+                    result = quote;
+                }
+            }
+        }
+
+        if (result == null) {
+            throw new FxConversionException("Подходящей котировки нет!");
+        }
+
+        boolean isSell = operation == ClientOperation.SELL;
+        return isSell ? result.getBid() : result.getOffer();
+    }
+
+    private int quoteComparison(Quote lq, Quote rq) {
+        if (lq.isInfinity()) {
+            return 1;
+        }
+
+        if (rq.isInfinity()) {
+            return -1;
+        }
+        return lq.getVolumeSize().compareTo(rq.getVolumeSize());
+    }
+}
+
+
