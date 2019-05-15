cbc81ac45f45554215d2cb4f620c3b6c526e3b41
Pelynthe <sigma.no.gemini@gmail.com>
2019-05-16 01:25:54 +0300

    Add converter

--- common/src/main/groovy/ru/sberbank/school/task03/model/ResponseReportModel.groovy
+++ common/src/main/groovy/ru/sberbank/school/task03/model/ResponseReportModel.groovy
@@ -1,4 +1,33 @@
-package ru.sberbank.school.task03.util
+package ru.sberbank.school.task03.model
 
 class ResponseReportModel {
+    int responsesQuantity
+    int maxResponsesBySymbol
+    String maxResponsesSymbol
+
+    List<String> modelSymbols = new ArrayList<>();
+    Map<String, ReportMiddleBlock> modelBlocks = new HashMap<>()
+
+    class ReportMiddleBlock {
+        String symbol
+        BigDecimal maxAmount
+        BigDecimal avgAmount
+        BigDecimal profitableAmount
+        BigDecimal unprofitableAmount
+        int symbolResponseCount
+        double minAmount
+        double profitablePrice
+        double unprofitablePrice
+
+        List<ReportResponse> modelResponses = new ArrayList<>()
+    }
+
+    class ReportResponse {
+        String date
+        String direction
+        BigDecimal amount
+        int number
+        double price
+        boolean isNotFound
+    }
 }
