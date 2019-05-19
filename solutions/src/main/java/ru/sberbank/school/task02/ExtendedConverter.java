101f72feea0c1489b2ff7820ed0b910057c26bd1
aloba <alobanova2144@gmail.com>
2019-05-19 10:37:52 +0300

    id 3. Реализация ОВК

--- solutions/src/main/java/ru/sberbank/school/task02/ExtendedConverter.java
+++ solutions/src/main/java/ru/sberbank/school/task02/ExtendedConverter.java
@@ -6,6 +6,7 @@ import ru.sberbank.school.task02.util.Quote;
 import ru.sberbank.school.task02.util.Symbol;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -26,52 +27,52 @@ public class ExtendedConverter extends Converter implements ExtendedFxConversion
 
         List<Quote> quotes = externalQuotesService.getQuotes(symbol);
 
-        Quote quote = null;
-        Quote reserveQuote = null;
+        List<Quote> quotesResult = new ArrayList<>();
 
         for (Quote currQuote : quotes) {
             BigDecimal price = getPrice(operation, currQuote);
-            BigDecimal usd = amount.divide(price, 15, BigDecimal.ROUND_HALF_UP);
+            BigDecimal amountBaseCurrency = amount.divide(price, 10, BigDecimal.ROUND_HALF_UP);
 
-            if (currQuote.getVolumeSize().compareTo(usd) > 0 || currQuote.isInfinity()) {
-                Quote quoteUsd = searchQuote(usd, quotes);
-                if (currQuote.getVolumeSize().compareTo(quoteUsd.getVolumeSize()) == 0) {
-                    if (quote == null) {
-                        quote = quoteUsd;
-                    } else {
-                        reserveQuote = quoteUsd;
-                    }
+            if (currQuote.getVolumeSize().compareTo(amountBaseCurrency) > 0 || currQuote.isInfinity()) {
+                Quote quoteBaseCurrency = searchQuote(amountBaseCurrency, quotes);
+                if (currQuote.getVolumeSize().compareTo(quoteBaseCurrency.getVolumeSize()) == 0) {
+                    quotesResult.add(currQuote);
                 }
             }
         }
 
-        if (quote == null) {
+        if (quotesResult.isEmpty()) {
             return Optional.empty();
         }
 
-        if (reserveQuote != null) {
-            quote = choiceBeneficiary(quote, reserveQuote, beneficiary);
-        }
+        BigDecimal price = choicePriceByBeneficiary(quotesResult, beneficiary, operation);
 
-        BigDecimal price = getPrice(operation, quote);
-
-        return Optional.ofNullable(BigDecimal.ONE.divide(price, 15, BigDecimal.ROUND_HALF_UP));
+        return Optional.ofNullable(BigDecimal.ONE.divide(price, 10, BigDecimal.ROUND_HALF_UP));
 
     }
 
-    private Quote choiceBeneficiary(Quote quote, Quote reserveQuote, Beneficiary beneficiary) {
-        if (reserveQuote.isInfinity()) {
-            return beneficiary == Beneficiary.CLIENT ? reserveQuote : quote;
+    private BigDecimal choicePriceByBeneficiary(List<Quote> quotes, Beneficiary beneficiary, ClientOperation operation) {
+        BigDecimal priceResult = getPrice(operation, quotes.get(0));
+
+        if (quotes.size() == 1) {
+            return priceResult;
         }
 
-        if (quote.isInfinity()) {
-            return beneficiary == Beneficiary.CLIENT ? quote : reserveQuote;
-        }
+        for (Quote quote : quotes) {
+            BigDecimal price = getPrice(operation, quote);
 
-        if (quote.getVolumeSize().compareTo(reserveQuote.getVolumeSize()) < 0) {
-            return beneficiary == Beneficiary.CLIENT ? reserveQuote : quote;
+            if (price.compareTo(priceResult) > 0) {
+                if (beneficiary == Beneficiary.BANK && operation == ClientOperation.BUY
+                        || beneficiary == Beneficiary.CLIENT && operation == ClientOperation.SELL) {
+                    priceResult = price;
+                }
+            } else {
+                if (beneficiary == Beneficiary.BANK && operation == ClientOperation.SELL
+                        || beneficiary == Beneficiary.CLIENT && operation == ClientOperation.BUY) {
+                    priceResult = price;
+                }
+            }
         }
-
-        return quote;
+        return priceResult;
     }
 }
