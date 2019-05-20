298ca49f4b013aba7d6bd23b50139b9fd4fd3311
Egor Bzhosko <lansxkeku@mail.ru>
2019-05-20 22:20:22 +0300

    id:2, take pull and push

---
+++ solutions/src/main/java/ru/sberbank/school/task02/service/Main.java
@@ -0,0 +1,21 @@
+package ru.sberbank.school.task02.service;
+
+
+import ru.sberbank.school.task02.util.ClientOperation;
+import ru.sberbank.school.task02.util.ExternalQuotesServiceDemo;
+import ru.sberbank.school.task02.util.Symbol;
+
+import java.math.BigDecimal;
+
+public class Main {
+    public static void main(String[] args) {
+
+        ServiceFactoryImpl serviceFactory = new ServiceFactoryImpl();
+
+
+        System.out.println(serviceFactory
+                            .getFxConversionService(
+                                    new ExternalQuotesServiceImpl())
+                            .convert(ClientOperation.BUY, Symbol.USD_RUB, BigDecimal.valueOf(10000000)));
+    }
+}
