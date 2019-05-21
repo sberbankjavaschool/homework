7fc8e82c5bc17c55476c26d8d65bae6b471b01c1
BahusDivus <bahusdivus@gmail.com>
2019-05-21 17:42:51 +0300

    Pull from origin:source

--- solutions/src/main/java/ru/sberbank/school/task02/ReverseCalculator.java
+++ solutions/src/main/java/ru/sberbank/school/task02/ReverseCalculator.java
@@ -8,6 +8,9 @@ import ru.sberbank.school.task02.util.Symbol;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
@@ -35,54 +38,42 @@ public class ReverseCalculator implements ExtendedFxConversionService {
         if (quotes.isEmpty()) {
             throw new FxConversionException("No quotes found");
         }
-        Quote current = null;
-        for (Quote quote: quotes) {
-            BigDecimal price = operation == ClientOperation.BUY ? quote.getBid() : quote.getOffer();
-            BigDecimal reverseAmount = amount.divide(price, 10, RoundingMode.HALF_UP);
-            current = calculator.checkQuote(reverseAmount, quote, current);
-        }
-        if (current == null) {
+
+        List<Quote> filteredQuotes = filterQuotes(amount, quotes, operation);
+
+        if (filteredQuotes.isEmpty()) {
             return Optional.empty();
         }
-        BigDecimal priceResult = operation == ClientOperation.BUY ? current.getBid() : current.getOffer();
-        BigDecimal revertResult = BigDecimal.ONE.divide(priceResult, 10, RoundingMode.HALF_UP);
-        return Optional.ofNullable(revertResult);
+        BigDecimal priceResult;
+        if (filteredQuotes.size() == 1) {
+            Quote current = filteredQuotes.get(0);
+            priceResult = operation == ClientOperation.BUY ? current.getBid() : current.getOffer();
+         } else {
+            if (operation == ClientOperation.BUY && beneficiary == Beneficiary.CLIENT) {
+                priceResult = Collections.min(filteredQuotes, Comparator.comparing(Quote::getBid)).getBid();
+            } else if (operation == ClientOperation.BUY && beneficiary == Beneficiary.BANK) {
+                priceResult = Collections.max(filteredQuotes, Comparator.comparing(Quote::getBid)).getBid();
+            } else if (operation == ClientOperation.SELL && beneficiary == Beneficiary.CLIENT) {
+                priceResult = Collections.max(filteredQuotes, Comparator.comparing(Quote::getOffer)).getOffer();
+            } else {
+                priceResult = Collections.min(filteredQuotes, Comparator.comparing(Quote::getOffer)).getOffer();
+            }
+        }
+        return Optional.ofNullable(BigDecimal.ONE.divide(priceResult, 10, RoundingMode.HALF_UP));
     }
 
     @Override
     public Optional<BigDecimal> convertReversed(ClientOperation operation, Symbol symbol, BigDecimal amount,
                                                 double delta, Beneficiary beneficiary) {
+
         if (delta < 0) {
             throw new IllegalArgumentException("Negative delta is mindless");
         }
-        Optional<BigDecimal> noDeltaResult = convertReversed(operation, symbol, amount, beneficiary);
-        if (noDeltaResult.isPresent() || delta == 0) {
-            return noDeltaResult;
-        } else {
-            Optional<BigDecimal> revertResultHighDelta = convertReversed(operation, symbol,
-                    amount.add(BigDecimal.valueOf(delta)), beneficiary);
-            Optional<BigDecimal> revertResultLowDelta = convertReversed(operation, symbol,
-                    amount.subtract(BigDecimal.valueOf(delta)), beneficiary);
-            if (!revertResultHighDelta.isPresent()) {
-                return revertResultLowDelta;
-            }
-            if (!revertResultLowDelta.isPresent()) {
-                return revertResultHighDelta;
-            }
-            if (revertResultHighDelta.get().compareTo(revertResultLowDelta.get()) > 0 ) {
-                if (operation == ClientOperation.BUY ^ beneficiary == Beneficiary.BANK) {
-                    return revertResultLowDelta;
-                } else {
-                    return revertResultHighDelta;
-                }
-            } else {
-                if (operation == ClientOperation.BUY ^ beneficiary == Beneficiary.BANK) {
-                    return revertResultHighDelta;
-                } else {
-                    return revertResultLowDelta;
-                }
-            }
+        if (delta == 0) {
+            return convertReversed(operation, symbol, amount, beneficiary);
         }
+
+        throw new UnsupportedOperationException();
     }
 
     @Override
@@ -90,8 +81,32 @@ public class ReverseCalculator implements ExtendedFxConversionService {
         return calculator.convert(operation, symbol, amount);
     }
 
+    private List<Quote> filterQuotes(BigDecimal amount, List<Quote> quotes, ClientOperation operation) {
+        quotes.sort((left, right) -> {
+            if (left.isInfinity()) {
+                return 1;
+            }
+            if (right.isInfinity()) {
+                return -1;
+            }
+            return left.getVolumeSize().compareTo(right.getVolumeSize());
+        });
+
+        List<Quote> filteredQuotes = new ArrayList<>();
+        BigDecimal lastVolume = BigDecimal.ZERO;
+        for (Quote quote : quotes) {
+            BigDecimal price = operation == ClientOperation.BUY ? quote.getBid() : quote.getOffer();
+            if (amount.compareTo(lastVolume.multiply(price)) >= 0
+                    && amount.compareTo(quote.getVolumeSize().multiply(price)) < 0) {
+                filteredQuotes.add(quote);
+            }
+        }
+        return filteredQuotes;
+    }
+
     private void validate(ClientOperation operation, Symbol symbol, BigDecimal amount, Beneficiary beneficiary) {
         calculator.validate(operation, symbol, amount);
         Objects.requireNonNull(beneficiary, "No beneficiary provided");
     }
+
 }
