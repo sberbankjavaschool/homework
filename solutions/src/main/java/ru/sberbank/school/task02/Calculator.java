97fbe907d8f5264c82eadac70372ce945954b8ac
Alex <al.kolmogorov@gmail.com>
2019-05-21 18:39:38 +0300

    Сквош всех заданий сданных на 21.05

---
+++ solutions/src/main/java/ru/sberbank/school/task02/Calculator.java
@@ -0,0 +1,59 @@
+package ru.sberbank.school.task02;
+
+import ru.sberbank.school.task02.exception.FxConversionException;
+import ru.sberbank.school.task02.util.ClientOperation;
+import ru.sberbank.school.task02.util.Quote;
+import ru.sberbank.school.task02.util.Symbol;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Objects;
+
+public class Calculator implements FxConversionService {
+    private ExternalQuotesService provider;
+
+    Calculator(ExternalQuotesService externalQuotesService) {
+        this.provider = externalQuotesService;
+    }
+
+    @Override
+    public BigDecimal convert(ClientOperation operation, Symbol symbol, BigDecimal amount) {
+        if (amount == null || operation == null || symbol == null) {
+            throw new NullPointerException();
+        }
+        if (amount.compareTo(BigDecimal.ZERO) <= 0) {
+            throw new IllegalArgumentException("Wrong amount");
+        }
+        List<Quote> quotes = provider.getQuotes(symbol);
+        if (quotes == null || quotes.isEmpty()) {
+            throw new FxConversionException("No quotes found");
+        }
+
+        Quote current = null;
+        for (Quote quote: quotes) {
+            current = checkQuote(amount, quote, current);
+        }
+        if (current == null) {
+            throw new FxConversionException("No suitable quote found");
+        }
+        return operation == ClientOperation.BUY ? current.getOffer() : current.getBid();
+    }
+
+    Quote checkQuote(BigDecimal amount, Quote quote, Quote current) {
+        if (quote.isInfinity() && current == null) {
+            current = quote;
+        } else if (amount.compareTo(quote.getVolumeSize()) < 0) {
+            if (current == null || current.isInfinity()
+                    || current.getVolumeSize().compareTo(quote.getVolumeSize()) > 0) {
+                current = quote;
+            }
+        }
+        return current;
+    }
+
+    void validate(ClientOperation operation, Symbol symbol, BigDecimal amount) {
+        Objects.requireNonNull(operation, "No operation provided");
+        Objects.requireNonNull(symbol, "No symbol provided");
+        Objects.requireNonNull(amount, "No amount provided");
+    }
+}
---
+++ solutions/src/test/java/ru/sberbank/school/task02/CalculatorTest.java
@@ -0,0 +1,93 @@
+package ru.sberbank.school.task02;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import ru.sberbank.school.task02.util.ClientOperation;
+import ru.sberbank.school.task02.util.ExternalQuotesServiceDemo;
+import ru.sberbank.school.task02.util.Symbol;
+
+import java.math.BigDecimal;
+
+class CalculatorTest {
+    private ServiceFactory factory = new ServiceFactoryImpl();
+    private ExternalQuotesService quotesService = new ExternalQuotesProvider();
+    private ExternalQuotesService demoQuotesService = new ExternalQuotesServiceDemo();
+
+    @Test
+    void convertReturnsMinBidIfLessThanFirstQuote() {
+        FxConversionService calculator = factory.getFxConversionService(quotesService);
+        BigDecimal offer = calculator.convert(ClientOperation.BUY, Symbol.USD_RUB, BigDecimal.valueOf(1));
+        Assertions.assertEquals(BigDecimal.valueOf(1000), offer);
+    }
+
+    @Test
+    void convertReturnsMaxBidIfGreaterThanLastQuote() {
+        FxConversionService calculator = factory.getFxConversionService(quotesService);
+        BigDecimal offer = calculator.convert(ClientOperation.BUY, Symbol.USD_RUB, BigDecimal.valueOf(111111));
+        Assertions.assertEquals(BigDecimal.valueOf(10000), offer);
+    }
+
+    @Test
+    void convertReturns2ndBidIfIn2ndQuote() {
+        FxConversionService calculator = factory.getFxConversionService(quotesService);
+        BigDecimal offer = calculator.convert(ClientOperation.BUY, Symbol.USD_RUB, BigDecimal.valueOf(11));
+        Assertions.assertEquals(BigDecimal.valueOf(2000), offer);
+    }
+
+    @Test
+    void convertReturns4rdBidIfEquals3rdQuoteVolumeSize() {
+        FxConversionService calculator = factory.getFxConversionService(quotesService);
+        BigDecimal offer = calculator.convert(ClientOperation.BUY, Symbol.USD_RUB, BigDecimal.valueOf(30));
+        Assertions.assertEquals(BigDecimal.valueOf(4000), offer);
+    }
+
+    @Test
+    void convertReturnsIfQuoteVolumeSizeDouble() {
+        FxConversionService calculator = factory.getFxConversionService(quotesService);
+        BigDecimal offer = calculator.convert(ClientOperation.BUY, Symbol.USD_RUB, BigDecimal.valueOf(31.0234234));
+        Assertions.assertEquals(BigDecimal.valueOf(4000), offer);
+    }
+
+    @Test
+    void convertIfProvidedNegativeAmount() {
+        FxConversionService calculator = factory.getFxConversionService(quotesService);
+        Assertions.assertThrows(IllegalArgumentException.class,
+                () -> calculator.convert(ClientOperation.BUY, Symbol.USD_RUB, BigDecimal.valueOf(-1)));
+    }
+
+    @Test
+    void convertIfProvidedZeroAmount() {
+        FxConversionService calculator = factory.getFxConversionService(quotesService);
+        Assertions.assertThrows(IllegalArgumentException.class,
+                () -> calculator.convert(ClientOperation.BUY, Symbol.USD_RUB, BigDecimal.valueOf(-1)));
+    }
+
+    @Test
+    void convertDemoProviderMoreThanMaxValue() {
+        FxConversionService calculator = factory.getFxConversionService(demoQuotesService);
+        BigDecimal offer = calculator.convert(ClientOperation.BUY, Symbol.USD_RUB, BigDecimal.valueOf(1_000_001));
+        Assertions.assertEquals(BigDecimal.valueOf(84), offer);
+    }
+
+    @Test
+    void convertDemoProviderMoreThan1000Double() {
+        FxConversionService calculator = factory.getFxConversionService(demoQuotesService);
+        BigDecimal offer = calculator.convert(ClientOperation.BUY, Symbol.USD_RUB,
+                BigDecimal.valueOf(1005.0410071948494));
+        Assertions.assertEquals(BigDecimal.valueOf(82), offer);
+    }
+
+    @Test
+    void convertDemoProviderMaxValue() {
+        FxConversionService calculator = factory.getFxConversionService(demoQuotesService);
+        BigDecimal offer = calculator.convert(ClientOperation.BUY, Symbol.USD_RUB, BigDecimal.valueOf(1_000_000));
+        Assertions.assertEquals(BigDecimal.valueOf(84), offer);
+    }
+
+    @Test
+    void convertDemoProviderMinValue() {
+        FxConversionService calculator = factory.getFxConversionService(demoQuotesService);
+        BigDecimal offer = calculator.convert(ClientOperation.BUY, Symbol.USD_RUB, BigDecimal.valueOf(10));
+        Assertions.assertEquals(BigDecimal.valueOf(85), offer);
+    }
+}
