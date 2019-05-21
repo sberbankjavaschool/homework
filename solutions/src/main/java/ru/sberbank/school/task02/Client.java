8534adbf42b7f93bd2918cb36d79845976f81194
Alex K <bormoshka@gmail.com>
2019-05-21 23:31:28 +0300

    Squashed on 21.05

---
+++ solutions/src/main/java/ru/sberbank/school/task02/Client.java
@@ -0,0 +1,123 @@
+package ru.sberbank.school.task02;
+
+import ru.sberbank.school.task02.exception.ConverterConfigurationException;
+import ru.sberbank.school.task02.exception.FxConversionException;
+import ru.sberbank.school.task02.util.*;
+
+import java.math.BigDecimal;
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+import java.text.ParseException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
+
+
+public class Client implements FxClientController {
+    private ExtendedFxConversionService calculator;
+    private Beneficiary beneficiary;
+
+    public Client(ExtendedFxConversionService calculator) {
+        this.calculator = Objects.requireNonNull(calculator, "ExtendedFxConversionService can't be null");
+
+        String beneficiary = System.getenv("SBRF_BENEFICIARY");
+
+        if (beneficiary == null || beneficiary.isEmpty()) {
+            throw new ConverterConfigurationException("Can't find environment variable 'SBRF_BENEFICIARY'!");
+        } else if (beneficiary.equalsIgnoreCase("BANK")) {
+            this.beneficiary = Beneficiary.BANK;
+        } else if (beneficiary.equalsIgnoreCase("CLIENT")) {
+            this.beneficiary = Beneficiary.CLIENT;
+        } else {
+            throw new ConverterConfigurationException("'SBRF_BENEFICIARY' must be BANK or CLIENT, current: "
+                    + beneficiary);
+        }
+    }
+
+    @Override
+    public List<FxResponse> fetchResult(List<FxRequest> requests) {
+        if (requests == null) {
+            throw new NullPointerException("List of requests mustn't be null");
+        }
+        if (requests.isEmpty()) {
+            throw new FxConversionException("List of requests mustn't be empty");
+        }
+
+        List<FxResponse> result = new ArrayList<>();
+
+        for (FxRequest request : requests) {
+            result.add(fetchResult(request));
+        }
+        return result;
+    }
+
+    @Override
+    public FxResponse fetchResult(FxRequest requests) {
+        if (requests == null) {
+            throw new NullPointerException("Request must not be null");
+        }
+
+        Symbol symbol = getSymbol(requests);
+        ClientOperation operation = getOperation(requests);
+        BigDecimal amount = getAmount(requests);
+
+        Optional<BigDecimal> result = calculator.convertReversed(operation, symbol, amount, this.beneficiary);
+        String price = format(result.isPresent() ? result.get() : null);
+
+        return new FxResponse(symbol.getSymbol(), price, format(amount),
+                getDateAndTime(), operation.toString(), !result.isPresent());
+    }
+
+    private String getDateAndTime() {
+        return LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm:ss"));
+    }
+
+    private String format(BigDecimal amount) {
+        DecimalFormatSymbols decimalSymbols = new DecimalFormatSymbols();
+        decimalSymbols.setDecimalSeparator('.');
+
+        return new DecimalFormat("#.##", decimalSymbols).format(amount);
+    }
+
+    private Symbol getSymbol(FxRequest requests) {
+        if (requests.getSymbol().isEmpty()) {
+            throw new FxConversionException("Whe haven't got Symbol");
+        } else if (requests.getSymbol().equalsIgnoreCase("USD/RUB")) {
+            return Symbol.USD_RUB;
+        } else if (requests.getSymbol().equalsIgnoreCase("RUB/USD")) {
+            return Symbol.RUB_USD;
+        } else {
+            throw new IllegalArgumentException("Symbols must be USD/RUB or RUB/USD, but current: "
+                    + requests.getSymbol());
+        }
+    }
+
+    private ClientOperation getOperation(FxRequest requests) {
+        if (requests.getDirection().isEmpty()) {
+            throw new FxConversionException("Whe haven't got operation");
+        } else if ((requests.getDirection().equalsIgnoreCase("BUY"))) {
+            return ClientOperation.BUY;
+        } else if ((requests.getDirection().equalsIgnoreCase("SELL"))) {
+            return ClientOperation.SELL;
+        } else {
+            throw new IllegalArgumentException("Operation must be BUY or SELL, but current: "
+                    + requests.getDirection());
+        }
+    }
+
+    private BigDecimal getAmount(FxRequest requests) {
+        if (requests.getAmount().isEmpty()) {
+            throw new FxConversionException("Whe haven't got amount");
+        } else {
+            try {
+                DecimalFormat decimalFormat = new DecimalFormat("#.#");
+                decimalFormat.setParseBigDecimal(true);
+                return  (BigDecimal) decimalFormat.parse(requests.getAmount());
+            } catch (ParseException exc) {
+                throw new IllegalArgumentException("Amount argument is not number");
+            }
+        }
+    }
+
+
+}
