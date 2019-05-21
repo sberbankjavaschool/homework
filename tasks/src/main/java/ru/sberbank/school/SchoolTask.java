b6d8a4ba7e85fe62a609f089a6b62c4bdcf47219
Домнин Алексей Дмитриевич <Domnin.A.Dm@sberbank.ru>
2019-05-21 11:02:33 +0300

    Collections homework

--- tasks/src/main/java/ru/sberbank/school/SchoolTask.java
+++ tasks/src/main/java/ru/sberbank/school/SchoolTask.java
@@ -10,7 +10,8 @@ public enum SchoolTask {
     FX_CALC_02_02("Вал. Калькулятор ч.2", 3),
     FX_CALC_02_03("Вал. Калькулятор ч.3", 4),
     FX_CALC_03_01("Форматтер", 5),
-    GENERICS_06("Generics", 6);
+    GENERICS_06("Generics", 6),
+    COLLECTIONS_07("Коллекции", 7);
 
     private final String name;
     private final int code;
