fc4012aacff6dc33fc825182464e06b2ab060ab2
Ganzalo <Ganzalo2010@gmail.com>
2019-05-23 15:58:31 +0300

    add base dz 6

---
+++ solutions/src/main/java/ru/sberbank/school/task06/CountMapImpl.java
@@ -0,0 +1,40 @@
+package ru.sberbank.school.task06;
+
+import java.util.Map;
+
+public class CountMapImpl implements CountMap {
+    @Override
+    public void add(Object o) {
+
+    }
+
+    @Override
+    public int getCount(Object o) {
+        return 0;
+    }
+
+    @Override
+    public int remove(Object o) {
+        return 0;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public void addAll(CountMap source) {
+
+    }
+
+    @Override
+    public Map toMap() {
+        return null;
+    }
+
+    @Override
+    public void toMap(Map destination) {
+
+    }
+}
