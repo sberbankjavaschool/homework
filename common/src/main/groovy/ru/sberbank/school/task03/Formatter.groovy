a8b5d9190808ca2ca5e9e41db111bc1e203c202d
Tanya Naumovets <naumovets.ts@gmail.com>
2019-05-26 01:36:44 +0300

    Взяла изменения из форка

--- common/src/main/groovy/ru/sberbank/school/task03/Formatter.groovy
+++ common/src/main/groovy/ru/sberbank/school/task03/Formatter.groovy
@@ -21,17 +21,17 @@ ${getBlock()}
 
     String getBlock() {
 
-        String block = ""
+        StringBuilder block = new StringBuilder()
 
         for (Map.Entry<String, ResponseGroup> entry in report.getResponseGroups()) {
             ResponseGroup group = entry.getValue()
-            block += """
+            block.append("""
 === Данные по инструменту: ${entry.getKey()} ===
 ${getQuotes(group)}
 Данные по суммам (макс/сред/мин) : ${group.getMaxAmount()} / ${group.getMediumAmount()} / ${group.getMinAmount()}
-${getStatisticsBuy(group)}
-${getStatisticsSell(group)}
-"""
+${getStatisticsBuy(group) ?: "Запросов на покупку не было!"}
+${getStatisticsSell(group)?: "Запросов на продажу не было!"}
+""")
         }
 
         block
@@ -46,39 +46,26 @@ ${getStatisticsSell(group)}
 
             quote += "| ${r.getDate()} | ${r.getAmount()} | ${r.getDirection()} | ${r.getPrice()} \n"
         }
-
         quote
     }
 
     String getStatisticsBuy(ResponseGroup responseGroup) {
 
-        String statistics = ""
-
         if (responseGroup.getBestBuyPrice() == null) {
-            statistics += "Запросов на покупку не было!"
+             return null
         }
-        else {
-            statistics += """Самая выгодная для клиента цена при покупке ${responseGroup.getBestBuyPrice()} на объеме ${responseGroup.getBestBuyAmount()}
+
+        """Самая выгодная для клиента цена при покупке ${responseGroup.getBestBuyPrice()} на объеме ${responseGroup.getBestBuyAmount()}
 Самая невыгодная для клиента цена при покупке ${responseGroup.getWorstBuyPrice()} на объеме ${responseGroup.getWorstBuyAmount()}"""
-        }
-
-        statistics
-
     }
 
     String getStatisticsSell(ResponseGroup responseGroup) {
 
-        String statistics = ""
-
         if (responseGroup.getBestSellPrice() == null) {
-            statistics += "Запросов на продажу не было!"
+            return null
         }
-        else {
-            statistics += """Самая выгодная для клиента цена при продаже ${responseGroup.getBestSellPrice()} на объеме ${responseGroup.getBestSellAmount()}
+        """Самая выгодная для клиента цена при продаже ${responseGroup.getBestSellPrice()} на объеме ${responseGroup.getBestSellAmount()}
 Самая невыгодная для клиента цена при продаже ${responseGroup.getWorstSellPrice()} на объеме ${responseGroup.getWorstSellAmount()}"""
-        }
-
-        statistics
     }
 
 }
