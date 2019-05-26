a22782fd5fca0aa82fbb90d39a673baca8295b72
anaroman <romacia2007@mail.ru>
2019-05-26 13:21:40 +0300

    id06-07 build

--- solutions/src/main/java/ru/sberbank/school/task08/KryoSerializer.java
+++ solutions/src/main/java/ru/sberbank/school/task08/KryoSerializer.java
@@ -3,6 +3,6 @@ package ru.sberbank.school.task08;
 import ru.sberbank.school.task08.state.GameObject;
 import ru.sberbank.school.task08.state.MapState;
 import sun.plugin2.message.Serializer;
-
-public class KryoSerializer implements Serializer<MapState<GameObject>> {
-}
+//
+//public class KryoSerializer implements Serializer<MapState<GameObject>> {
+//}
