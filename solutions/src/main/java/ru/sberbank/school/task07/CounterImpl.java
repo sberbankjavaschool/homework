e96c1950c36892eb29b4e1d452f3dcafd8a33e56
anaroman <romacia2007@mail.ru>
2019-05-26 20:37:40 +0300

    id07

--- solutions/src/main/java/ru/sberbank/school/task07/CounterImpl.java
+++ solutions/src/main/java/ru/sberbank/school/task07/CounterImpl.java
@@ -23,7 +23,8 @@ public class CounterImpl implements Counter {
         List<String> words = fileParser.parse(pathToFile);
         HashSet<String> wordsMap = new HashSet<>();
         for (String word : words) {
-            wordsMap.addAll(Arrays.asList(word.toLowerCase().trim().split("\\s")));
+            List<String> listwords = Arrays.asList(word.toLowerCase().trim().split("\\s"));
+            wordsMap.addAll(listwords);
         }
         return wordsMap.size();
     }
