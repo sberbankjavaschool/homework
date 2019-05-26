3d00f6b6331113f12010f04c825691b366888317
Pelynthe <sigma.no.gemini@gmail.com>
2019-05-26 22:02:31 +0300

    task07

---
+++ solutions/src/main/java/ru/sberbank/school/task07/DifferentWordsImpl.java
@@ -0,0 +1,57 @@
+package ru.sberbank.school.task07;
+
+import lombok.RequiredArgsConstructor;
+
+import java.io.FileNotFoundException;
+import java.util.*;
+
+/**
+ * 26.05.2019
+ * Формирует список слов из файла, отсортированный по возрастанию их длины
+ *
+ * @author Gregory Melnikov
+ */
+
+@RequiredArgsConstructor
+public class DifferentWordsImpl implements DifferentWords {
+
+    private final FileParser fileParser;
+
+    private final String splitRegex = "[^a-zA-Zа-яА-Я0-9]|\\s+";
+
+    @Override
+    public Set<String> findSortedDifferentWords(String pathToFile) throws FileNotFoundException {
+
+        List<String> parsedStrings = fileParser.parse(pathToFile);
+
+        Set<String> resultSet = new TreeSet<>(getComparator());
+
+        for (String string : parsedStrings) {
+            String[] stringParts = string.split(splitRegex);
+            resultSet.addAll(Arrays.asList(stringParts));
+        }
+        return resultSet;
+    }
+
+    private Comparator<String> getComparator() {
+        Comparator<String> comparator = new Comparator<String>() {
+            @Override
+            public int compare(String s1, String s2) {
+                if (s1.length() > s2.length()) {
+                    return 1;
+                } else if (s1.length() < s2.length()) {
+                    return -1;
+                } else {
+                    if (s1.compareTo(s2) > 0) {
+                        return 1;
+                    }
+                    if (s1.compareTo(s2) < 0) {
+                        return -1;
+                    }
+                }
+                return 0;
+            }
+        };
+        return comparator;
+    }
+}
---
+++ solutions/src/test/java/ru/sberbank/school/task07/ReverseOrderIteratorImplTest.java
@@ -0,0 +1,41 @@
+package ru.sberbank.school.task07;
+
+import org.junit.Test;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ReverseOrderIteratorImplTest {
+
+    private final FileParser fileParser = new FileParserImpl();
+
+    private ReverseIteratorImpl reverseOrderIterator = null;
+
+    @Test
+    public void reverseOrderIteratorTest() {
+        File file = new File("src/test/resources/task07/CounterTest.txt");
+        String pathToFile = file.getAbsolutePath();
+        System.out.println(pathToFile);
+
+        List<String> list = new ArrayList<>();
+        try {
+            list = fileParser.parse(pathToFile);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        reverseOrderIterator = new ReverseIteratorImpl(list);
+
+        String target = "string4 test text";
+
+        assertEquals(target, reverseOrderIterator.next());
+
+        while (reverseOrderIterator.hasNext()) {
+            System.out.println(reverseOrderIterator.next());
+        }
+    }
+}
