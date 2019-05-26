11b8b4a1436f6c91414c7ae792ba3ce1facdf14a
anaroman <romacia2007@mail.ru>
2019-05-26 13:18:02 +0300

    id06-07

---
+++ solutions/src/main/java/ru/sberbank/school/task07/ReverseOrderImpl.java
@@ -0,0 +1,25 @@
+package ru.sberbank.school.task07;
+
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Задание: Соберите все строки файла в обратном порядке и верните их в подходящей коллекции. В
+ * качестве T выберите наиболее подходящую коллекцию.
+ */
+public class ReverseOrderImpl implements ReverseOrder {
+    private FileParser fileParser;
+
+    public ReverseOrderImpl(FileParser fileParser) {
+        this.fileParser = fileParser;
+    }
+
+    public List<String> getReverseOrderedStrings(String pathToFile) throws FileNotFoundException {
+        List<String> reversedList = new ArrayList<>();
+        reversedList = fileParser.parse(pathToFile);
+        Collections.reverse(reversedList);
+        return reversedList;
+    }
+}
