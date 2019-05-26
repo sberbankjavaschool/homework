963717c20ab9b407cfb364cb523b92e47bbcc9ae
anaroman <romacia2007@mail.ru>
2019-05-26 18:10:13 +0300

    id07 fix reviews

--- solutions/src/main/java/ru/sberbank/school/task08/KryoSerializer.java
+++ solutions/src/main/java/ru/sberbank/school/task08/KryoSerializer.java
@@ -1,8 +1,43 @@
 package ru.sberbank.school.task08;
 
-//import ru.sberbank.school.task08.state.GameObject;
-//import ru.sberbank.school.task08.state.MapState;
-//import sun.plugin2.message.Serializer;
-//
-//public class KryoSerializer implements Serializer<MapState<GameObject>> {
-//}
+import ru.sberbank.school.task08.state.GameObject;
+import ru.sberbank.school.task08.state.InstantiatableEntity;
+import ru.sberbank.school.task08.state.MapState;
+
+import com.esotericsoftware.kryo.Kryo;
+import com.esotericsoftware.kryo.Serializer;
+import com.esotericsoftware.kryo.io.Input;
+import com.esotericsoftware.kryo.io.Output;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class KryoSerializer extends Serializer<MapState<GameObject>> {
+
+    @Override
+    public void write(Kryo kryo, Output output, MapState<GameObject> object) {
+        output.write(object.getGameObjects().size());
+        output.writeString(object.getName());
+
+        for (GameObject go : object.getGameObjects()) {
+            output.writeString(go.getType().name());
+            output.writeString(go.getStatus().name());
+            output.writeLong(go.getHitPoints());
+        }
+
+    }
+
+    @Override
+    public MapState<GameObject> read(Kryo kryo, Input input, Class<? extends MapState<GameObject>> type) {
+        int size = input.readInt();
+        String name = input.readString();
+        List<GameObject> objectsList = new ArrayList<>();
+        for (int i = 0; i<size; i++) {
+            InstantiatableEntity.Status status = InstantiatableEntity.Status.valueOf(input.readString());
+            InstantiatableEntity.Type typeIE = InstantiatableEntity.Type.valueOf(input.readString());
+            Long hitPoints = input.readLong();
+            objectsList.add(i, new GameObject(typeIE, status, hitPoints));
+        }
+        return new MapState<>(name, objectsList);
+    }
+}
