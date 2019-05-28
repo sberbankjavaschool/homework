6d15b0d2ac8473261edbb6422006c30f288b31f5
anatennis <anatennis15@mail.ru>
2019-05-28 16:10:14 +0300

    (8) Romanova (#258)
    * fix conflicts
    
    * id08 add Exceptions
    
    * id08 add NonNull
    
    * id08 Kryo fix
    
    * id08 add JTests
    
    * id08 fix checkstyle

--- solutions/src/main/java/ru/sberbank/school/task08/KryoSerializer.java
+++ solutions/src/main/java/ru/sberbank/school/task08/KryoSerializer.java
@@ -1,5 +1,6 @@
 package ru.sberbank.school.task08;
 
+import lombok.NonNull;
 import ru.sberbank.school.task08.state.GameObject;
 import ru.sberbank.school.task08.state.InstantiatableEntity;
 import ru.sberbank.school.task08.state.MapState;
@@ -15,29 +16,15 @@ import java.util.List;
 public class KryoSerializer extends Serializer<MapState<GameObject>> {
 
     @Override
-    public void write(Kryo kryo, Output output, MapState<GameObject> object) {
-        output.write(object.getGameObjects().size());
+    public void write(Kryo kryo, @NonNull Output output, @NonNull MapState<GameObject> object) {
         output.writeString(object.getName());
-
-        for (GameObject go : object.getGameObjects()) {
-            output.writeString(go.getType().name());
-            output.writeString(go.getStatus().name());
-            output.writeLong(go.getHitPoints());
-        }
-
+        kryo.writeObjectOrNull(output, object.getGameObjects(), ArrayList.class);
     }
 
     @Override
-    public MapState<GameObject> read(Kryo kryo, Input input, Class<? extends MapState<GameObject>> type) {
-        int size = input.readInt();
+    public MapState<GameObject> read(Kryo kryo, @NonNull Input input, Class<? extends MapState<GameObject>> type) {
         String name = input.readString();
-        List<GameObject> objectsList = new ArrayList<>();
-        for (int i = 0; i < size; i++) {
-            InstantiatableEntity.Status status = InstantiatableEntity.Status.valueOf(input.readString());
-            InstantiatableEntity.Type typeIE = InstantiatableEntity.Type.valueOf(input.readString());
-            Long hitPoints = input.readLong();
-            objectsList.add(i, new GameObject(typeIE, status, hitPoints));
-        }
-        return new MapState<>(name, objectsList);
+        List<GameObject> gameObjects = kryo.readObjectOrNull(input, ArrayList.class);
+        return new MapState<>(name, gameObjects);
     }
 }
