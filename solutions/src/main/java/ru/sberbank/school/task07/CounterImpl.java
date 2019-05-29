ab29711646f15c6ed54a8fef40984774ba06a231
BahusDivus <bahusdivus@gmail.com>
2019-05-29 17:32:59 +0300

    Fixes

--- solutions/src/main/java/ru/sberbank/school/task07/CounterImpl.java
+++ solutions/src/main/java/ru/sberbank/school/task07/CounterImpl.java
@@ -2,7 +2,9 @@ package ru.sberbank.school.task07;
 
 import java.io.FileNotFoundException;
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class CounterImpl implements Counter {
     private FileParser fileParser;
@@ -14,8 +16,8 @@ public class CounterImpl implements Counter {
     @Override
     public int count(String pathToFile) throws FileNotFoundException {
         List<String> strings = fileParser.parse(pathToFile);
-        final Integer[] count = {0};
-        strings.forEach(string -> Arrays.asList(string.split("\\P{Alpha}+")).forEach(word -> count[0]++));
-        return count[0];
+        final Set<String> count = new HashSet<>();
+        strings.forEach(string -> count.addAll(Arrays.asList(string.split("\\P{Alpha}+"))));
+        return count.size();
     }
 }
--- solutions/src/test/java/ru/sberbank/school/task07/DifferentWordsImplTest.java
+++ solutions/src/test/java/ru/sberbank/school/task07/DifferentWordsImplTest.java
@@ -2,13 +2,10 @@ package ru.sberbank.school.task07;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.mockito.Matchers;
 import org.mockito.Mockito;
 
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 class DifferentWordsImplTest {
     private final FileParser fileParser = Mockito.mock(FileParserImpl.class);
@@ -21,14 +18,11 @@ class DifferentWordsImplTest {
         strings.add("one two three");
         strings.add("one two");
         strings.add("three");
-        Mockito.when(fileParser.parse(Matchers.anyString())).thenReturn(strings);
+        Mockito.when(fileParser.parse(Mockito.anyString())).thenReturn(strings);
 
-        List<String> expectedWords = Arrays.asList("zero", "one", "two", "three");
-        List<String> words = counter.findSortedDifferentWords("some path");
+        String expectedWords = Arrays.asList("one", "two", "zero", "three").toString();
+        Set<String> words = counter.findSortedDifferentWords("some path");
 
-        Assertions.assertAll(
-                () -> Assertions.assertTrue(expectedWords.containsAll(words)),
-                () -> Assertions.assertTrue(words.containsAll(expectedWords))
-        );
+        Assertions.assertEquals(expectedWords, words.toString());
     }
 }
