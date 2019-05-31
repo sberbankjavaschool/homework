6fc64186eeaeb9ada1c4614cf6bac118f34f877e
Vladimir Malygin <work.malygin@gmail.com>
2019-05-31 18:48:55 +0300

    Сериализация. Задача 08

---
+++ solutions/src/main/java/ru/sberbank/school/task08/MapStateSerializer.java
@@ -0,0 +1,28 @@
+package ru.sberbank.school.task08;
+
+import com.esotericsoftware.kryo.Kryo;
+import com.esotericsoftware.kryo.Serializer;
+import com.esotericsoftware.kryo.io.Input;
+import com.esotericsoftware.kryo.io.Output;
+import ru.sberbank.school.task08.state.GameObject;
+import ru.sberbank.school.task08.state.MapState;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MapStateSerializer extends Serializer<MapState<GameObject>> {
+
+    @Override
+    public void write(Kryo kryo, Output output, MapState<GameObject> object) {
+        output.writeString(object.getName());
+        kryo.writeObjectOrNull(output, object.getGameObjects(), ArrayList.class);
+    }
+
+    @Override
+    public MapState<GameObject> read(Kryo kryo, Input input, Class<? extends MapState<GameObject>> type) {
+        String name = input.readString();
+        List<GameObject> list = (List<GameObject>) kryo.readObjectOrNull(input, ArrayList.class);
+        return new MapState<>(name, list);
+    }
+
+}
---
+++ solutions/src/test/java/ru/sberbank/school/task08/SerializableManagerTest.java
@@ -0,0 +1,43 @@
+package ru.sberbank.school.task08;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import ru.sberbank.school.task08.state.GameObject;
+import ru.sberbank.school.task08.state.InstantiatableEntity;
+import ru.sberbank.school.task08.state.MapState;
+import ru.sberbank.school.task08.state.Savable;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SerializableManagerTest {
+
+    private static SerializableManager manager;
+    private static MapState<GameObject> mapState;
+    private static String filePath = "./src/test/java/ru/sberbank/school/task08";
+    private static String fileName = "serializable";
+
+    @Before
+    public void init() {
+        manager = new SerializableManager(filePath);
+        manager.initialize();
+        List<GameObject> gameObjectList = new ArrayList<>();
+        gameObjectList.add((GameObject) manager
+                .createEntity(InstantiatableEntity.Type.ENEMY, InstantiatableEntity.Status.KILLED, 0));
+        gameObjectList.add((GameObject) manager
+                .createEntity(InstantiatableEntity.Type.BUILDING, InstantiatableEntity.Status.SPAWNED, 50));
+        gameObjectList.add((GameObject) manager
+                .createEntity(InstantiatableEntity.Type.NPC, InstantiatableEntity.Status.KILLED, 0));
+        mapState = manager.createSavable("Test state", gameObjectList);
+    }
+
+    @Test
+    @DisplayName("Equals test")
+    public void equalsTest() throws SaveGameException {
+        manager.saveGame(fileName, mapState);
+        Savable loadedObject = manager.loadGame(fileName);
+        Assertions.assertEquals(mapState, loadedObject);
+    }
+}
