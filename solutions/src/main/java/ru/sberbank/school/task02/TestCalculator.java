c4ddffd7108e628b5912bd682b0e6b4e1d1ceef4
Alex K <bormoshka@gmail.com>
2019-05-31 10:30:52 +0300

    SQUASH WITH REVERTS 31.05

---
+++ solutions/src/main/java/ru/sberbank/school/task02/TestCalculator.java
@@ -0,0 +1,27 @@
+package ru.sberbank.school.task02;
+
+import ru.sberbank.school.task02.services.MyExternalQuotesService;
+import ru.sberbank.school.task02.services.ServiceFactoryImpl;
+import ru.sberbank.school.task02.util.Beneficiary;
+import ru.sberbank.school.task02.util.ClientOperation;
+import ru.sberbank.school.task02.util.Symbol;
+
+import java.math.BigDecimal;
+
+public class TestCalculator {
+
+    public static void main(String[] args) {
+        ServiceFactory serviceFactory = new ServiceFactoryImpl();
+        ExternalQuotesService quotesService = new MyExternalQuotesService();
+
+        ExtendedFxConversionService calculator = serviceFactory.getExtendedFxConversionService(quotesService);
+
+        System.out.println(calculator.convertReversed(ClientOperation.BUY, Symbol.USD_RUB,
+                new BigDecimal(79_000_000),10,  Beneficiary.BANK));
+
+        System.out.println(calculator.convertReversed(ClientOperation.SELL, Symbol.USD_RUB,
+                new BigDecimal(99_000), 0,Beneficiary.CLIENT));
+
+    }
+
+}
---
+++ solutions/src/test/java/ru/sberbank/school/task08/JaxbManagerTest.java
@@ -0,0 +1,71 @@
+package ru.sberbank.school.task08;
+
+import org.junit.jupiter.api.*;
+import ru.sberbank.school.task08.state.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class JaxbManagerTest {
+    private static JaxbManager jaxbManager;
+    private static JaxbMapState<GameObject> mapState;
+    private static String directoryPath = System.getenv("directoryPath");
+    private String fileName = "Hospital.txt";
+    private static List<GameObject> objects;
+
+    @BeforeAll
+    static void init() {
+        jaxbManager = new JaxbManager(directoryPath);
+        jaxbManager.initialize();
+
+        objects = new ArrayList<>();
+        objects.add(jaxbManager.createEntity(InstantiatableEntity.Type.BUILDING,
+                InstantiatableEntity.Status.DESPAWNED, 10));
+        objects.add(jaxbManager.createEntity(InstantiatableEntity.Type.BUILDING,
+                InstantiatableEntity.Status.KILLED, 500));
+        objects.add(jaxbManager.createEntity(InstantiatableEntity.Type.NPC,
+                InstantiatableEntity.Status.SPAWNED, 0));
+        objects.add(jaxbManager.createEntity(InstantiatableEntity.Type.ENEMY,
+                InstantiatableEntity.Status.SPAWNED, 0));
+
+        mapState = jaxbManager.createSavable("Госпиталь", objects);
+    }
+
+    @Test
+    @DisplayName("Проверка на выброс NullPointerException")
+    void saveGameNull() {
+        Assertions.assertThrows(NullPointerException.class, () -> jaxbManager.saveGame(null, mapState));
+        Assertions.assertThrows(NullPointerException.class, () -> jaxbManager.saveGame(fileName, null));
+    }
+
+    @Test
+    @DisplayName("Проверка на выброс SaveGameException при некорректном пути к файлам сохранений")
+    void saveGameFileNotFound() {
+        jaxbManager = new JaxbManager("");
+        Assertions.assertThrows(SaveGameException.class, () -> jaxbManager.saveGame(fileName, mapState));
+        jaxbManager = new JaxbManager(directoryPath);
+    }
+
+    @Test
+    @DisplayName("Проверка на эквивалентность и неидентичность объектов")
+    void equalsSaveLoadObjects() throws SaveGameException {
+        jaxbManager.saveGame(fileName, mapState);
+        JaxbMapState<GameObject> otherMapState = jaxbManager.loadGame(fileName);
+
+        Assertions.assertNotSame(mapState, otherMapState);
+        Assertions.assertEquals(mapState, otherMapState);
+    }
+
+    @Test
+    @DisplayName("Проверка на эквивалентность и неиндетичность объектов при пустом списке")
+    void emptyListObjects() throws SaveGameException {
+        mapState = jaxbManager.createSavable("Пустой список", new ArrayList<>());
+        jaxbManager.saveGame(fileName, mapState);
+        JaxbMapState<GameObject> otherMapState = jaxbManager.loadGame(fileName);
+
+        Assertions.assertNotSame(mapState, otherMapState);
+        Assertions.assertEquals(mapState, otherMapState);
+
+        mapState = jaxbManager.createSavable("Госпиталь", objects);
+    }
+}
