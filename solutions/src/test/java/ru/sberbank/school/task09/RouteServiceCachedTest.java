24ab70e9cfa45796200e980bd2dcdc86e0394760
anatennis <anatennis15@mail.ru>
2019-06-01 10:07:55 +0300

    (9) Romanova (#271)
    * Squash 22.05
    
    * id06-07
    
    * id06-07 build
    
    * id06-07 build2
    
    * id06-07 checkstyle
    
    * id06 checkstyle
    
    * id07 fix reviews
    
    * id07 fix CountImpl mistakes
    
    * id07 fix CountImpl&DifWords mistakes
    
    * id07 fix DifWords mistakes
    
    * id07 fix DifWords mistakes2
    
    * id07 fix DifWords mistakes3
    
    * id07 fix CountWords mistakes
    
    * id07
    
    * id07 WordFreq fix
    
    * id07 WordFreq fix2
    
    * fix conflicts
    
    * id08 add Exceptions
    
    * id08 add NonNull
    
    * id08 Kryo fix
    
    * id08 add JTests
    
    * id08 fix checkstyle
    
    * (8) Romanova (#258)
    
    * fix conflicts
    
    * id08 add Exceptions
    
    * id08 add NonNull
    
    * id08 Kryo fix
    
    * id08 add JTests
    
    * id08 fix checkstyle
    
    * id09
    
    * Squash 22.05
    
    * id06-07
    
    * id06-07 build
    
    * id06-07 build2
    
    * id06-07 checkstyle
    
    * id06 checkstyle
    
    * id07 fix reviews
    
    * id07 fix CountImpl mistakes
    
    * id07 fix CountImpl&DifWords mistakes
    
    * id07 fix DifWords mistakes
    
    * id07 fix DifWords mistakes2
    
    * id07 fix DifWords mistakes3
    
    * id07 fix CountWords mistakes
    
    * id07
    
    * id07 WordFreq fix
    
    * id07 WordFreq fix2
    
    * (8) Romanova (#258)
    
    * fix conflicts
    
    * id08 add Exceptions
    
    * id08 add NonNull
    
    * id08 Kryo fix
    
    * id08 add JTests
    
    * id08 fix checkstyle
    
    * id09 add NonNull & stylefix
    
    * id09 add isEmpty
    
    * id09 try to fix kryo
    
    * id09 try to fix2
    
    * id09
    
    * id09 add annotation
    
    * id09 change constructor
    
    * id09 fix mistake
    
    * id09 del super
    
    * id09 add CitySerlztn
    
    * id09 com mist
    
    * id09 CitySerlz
    
    * id09 CitySerlz2
    
    * id09 1 more time
    
    * id09 smth

---
+++ solutions/src/test/java/ru/sberbank/school/task09/RouteServiceCachedTest.java
@@ -0,0 +1,41 @@
+package ru.sberbank.school.task09;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RouteServiceCachedTest {
+    private static RouteServiceCached routeServiceCached;
+    private static String path = "C:\\Users\\Anastasia\\Desktop\\Java\\task09Ser";
+
+    @BeforeAll
+    public static void initialization() {
+        routeServiceCached = new RouteServiceCached(path);
+    }
+
+    @Test
+    @DisplayName("Test Route equals")
+    public void equalsRoutesTest() {
+        Route<City> route = routeServiceCached.getRoute("Chelyabinsk", "Moscow");
+        Route<City> route_2 = routeServiceCached.getRoute("Chelyabinsk", "Moscow");
+        Assertions.assertEquals(route.getRouteName(), route_2.getRouteName());
+        Assertions.assertEquals(route.getCities().get(0), route_2.getCities().get(0));
+        Assertions.assertIterableEquals(route.getCities(), route_2.getCities());
+    }
+
+    @Test
+    @DisplayName("Timecheck")
+    void timeCheck() {
+        long startTime = System.nanoTime();
+        System.out.println("Start_time " + startTime);
+        Route<City> route_2 = routeServiceCached.getRoute("Chelyabinsk", "Moscow");
+        System.out.println("After serialization " + (System.nanoTime() - startTime));
+        Route<City> route = routeServiceCached.getRoute("Chelyabinsk", "Moscow");
+        System.out.println("After deserialization " + (System.nanoTime() - startTime));
+    }
+
+
+}
