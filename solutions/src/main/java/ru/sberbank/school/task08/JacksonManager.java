6936e982ac923f29b93375605209115dce70a9c4
Pelynthe <sigma.no.gemini@gmail.com>
2019-06-01 23:02:40 +0300

    task08: squash task08: null test, writeObject -> writeObjectOrNull task08: GameObject cast task08: удалены System.err.println(e); task08: notExistFile tests task08: минорные правки task08: JacksonManager task08: KryoManager + SerializableManager

--- solutions/src/main/java/ru/sberbank/school/task08/JacksonManager.java
+++ solutions/src/main/java/ru/sberbank/school/task08/JacksonManager.java
@@ -1,5 +1,6 @@
 package ru.sberbank.school.task08;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.NonNull;
 import ru.sberbank.school.task08.state.GameObject;
 import ru.sberbank.school.task08.state.InstantiatableEntity;
@@ -12,31 +13,35 @@ import java.util.List;
 import static java.io.File.separator;
 import static ru.sberbank.school.task08.SaveGameException.Type.IO;
 
-@Solution(8)
-public class SerializableManager extends SaveGameManager<MapState<GameObject>, GameObject> {
+/**
+ * 31.05.2019 сериализация с помощью Jackson
+ *
+ * @author Gregory Melnikov
+ */
 
-    /**
-     * Конструктор не меняйте.
-     */
-    public SerializableManager(@NonNull String filesDirectoryPath) {
+@Solution(8)
+public class JacksonManager extends SaveGameManager<MapState<GameObject>, GameObject> {
+
+    private ObjectMapper objectMapper;
+
+    public JacksonManager(@NonNull String filesDirectoryPath) {
         super(filesDirectoryPath);
     }
 
     @Override
     public void initialize() {
-//        throw new UnsupportedOperationException("Implement me!");
+        objectMapper = new ObjectMapper();
     }
 
     @Override
     public void saveGame(@NonNull String filename, @NonNull MapState<GameObject> gameState) throws SaveGameException {
         String path = filesDirectory + separator + filename;
+        try (FileOutputStream fileOutputStream = new FileOutputStream(path)) {
 
-        try (ObjectOutputStream outputStream = new ObjectOutputStream(new FileOutputStream(path))) {
-
-            outputStream.writeObject(gameState);
+            objectMapper.writeValue(fileOutputStream, gameState);
 
         } catch (IOException | NullPointerException e) {
-            throw new SaveGameException("SerializableManager Saving error", e, IO, gameState);
+            throw new SaveGameException("JacksonManager Saving error", e, IO, gameState);
         }
     }
 
@@ -44,20 +49,19 @@ public class SerializableManager extends SaveGameManager<MapState<GameObject>, G
     @SuppressWarnings("unchecked")
     public MapState<GameObject> loadGame(@NonNull String filename) throws SaveGameException {
         String path = filesDirectory + separator + filename;
+        try (FileInputStream fileInputStream = new FileInputStream(path)) {
 
-        try (ObjectInputStream inputStream = new ObjectInputStream(new FileInputStream(path))) {
-
-            MapState<GameObject> saved = (MapState<GameObject>) inputStream.readObject();
+            MapState<GameObject> saved = objectMapper.readValue(fileInputStream, MapState.class);
             return saved;
 
-        } catch (IOException | NullPointerException | ClassNotFoundException | ClassCastException e) {
-            throw new SaveGameException("SerializableManager Loading error", e, IO, null);
+        } catch (IOException | NullPointerException | ClassCastException e) {
+            throw new SaveGameException("JacksonManager Loading error", e, IO, null);
         }
     }
 
     @Override
-    public GameObject createEntity(InstantiatableEntity .Type type,
-                                             InstantiatableEntity .Status status,
+    public GameObject createEntity(InstantiatableEntity.Type type,
+                                             InstantiatableEntity.Status status,
                                              long hitPoints) {
         return new GameObject(type, status, hitPoints);
     }
@@ -65,6 +69,6 @@ public class SerializableManager extends SaveGameManager<MapState<GameObject>, G
     @Override
     @SuppressWarnings("unchecked")
     public MapState<GameObject> createSavable(String name, List<GameObject> entities) {
-        return new MapState<GameObject>(name, entities);
+        return new MapState<>(name, entities);
     }
 }
---
+++ solutions/src/test/java/ru/sberbank/school/task08/SerializableManagerTest.java
@@ -0,0 +1,80 @@
+package ru.sberbank.school.task08;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import ru.sberbank.school.task08.state.GameObject;
+import ru.sberbank.school.task08.state.MapState;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+import static ru.sberbank.school.task08.state.InstantiatableEntity.Status.*;
+import static ru.sberbank.school.task08.state.InstantiatableEntity.Type.*;
+
+class SerializableManagerTest {
+
+    private MapState<GameObject> mapState;
+    private List<GameObject> gameObjects = new ArrayList<>();
+    private String fileName = "SerializableFile.bin";
+    private String filesDirectoryPath = new File("src/test/resources/task08").getAbsolutePath();
+    private SerializableManager manager = new SerializableManager(filesDirectoryPath);
+
+    {
+        gameObjects.add(manager.createEntity(NPC, SPAWNED, 50));
+        gameObjects.add(manager.createEntity(NPC, DESPAWNED, 0));
+        gameObjects.add(manager.createEntity(ENEMY, SPAWNED, 100));
+        gameObjects.add(manager.createEntity(ENEMY, KILLED, 0));
+        gameObjects.add(manager.createEntity(ITEM, SPAWNED, 10));
+        gameObjects.add(manager.createEntity(BUILDING, SPAWNED, 1000));
+
+        mapState = manager.createSavable("level33", gameObjects);
+        manager.initialize();
+    }
+
+    @Test
+    void saveGamePositiveTest() throws SaveGameException {
+        manager.saveGame(fileName, mapState);
+    }
+
+    @Test
+    void loadGamePositiveTest() throws SaveGameException {
+        manager.saveGame(fileName, mapState);
+        manager.loadGame(fileName);
+    }
+
+    @Test
+    void loadGameFileNotExistTest() throws SaveGameException {
+        String notExistFile = "NotExistFile.json";
+        Assertions.assertThrows(SaveGameException.class, () -> manager.loadGame(notExistFile));
+    }
+
+    @Test
+    void saveLoadGameEqualsTest() throws SaveGameException {
+        manager.saveGame(fileName, mapState);
+        MapState<GameObject> otherMapState = manager.loadGame(fileName);
+
+        Assertions.assertEquals(mapState, otherMapState);
+        System.out.println(mapState.toString());
+        System.out.println(otherMapState.toString());
+    }
+
+    @Test
+    void saveLoadGameNotSameTest() throws SaveGameException {
+        manager.saveGame(fileName, mapState);
+        MapState<GameObject> otherMapState = manager.loadGame(fileName);
+
+        Assertions.assertNotSame(mapState, otherMapState);
+    }
+
+    @Test
+    void saveLoadGameNullObjectsTest() throws SaveGameException {
+        MapState localMapState = manager.createSavable("level13", null);
+
+        manager.saveGame(fileName, localMapState);
+        MapState<GameObject> otherMapState = manager.loadGame(fileName);
+
+        Assertions.assertEquals(localMapState, otherMapState);
+        Assertions.assertNotSame(localMapState, otherMapState);
+    }
+}
