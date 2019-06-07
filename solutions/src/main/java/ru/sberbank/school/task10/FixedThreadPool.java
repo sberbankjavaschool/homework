1d8eba3bfbe15dafbcd80cafe8b5f3b294e73b6f
anatennis <anatennis15@mail.ru>
2019-06-07 14:21:56 +0300

    (10) Romanova (#288)
    * Squash 22.05
    
    * id06-07
    
    * id06-07 build
    
    * id06-07 build2
    
    * id06-07 checkstyle
    
    * id06 checkstyle
    
    * id07 fix reviews
    
    * id07 fix CountImpl mistakes
    
    * id07 fix CountImpl&DifWords mistakes
    
    * id07 fix DifWords mistakes
    
    * id07 fix DifWords mistakes2
    
    * id07 fix DifWords mistakes3
    
    * id07 fix CountWords mistakes
    
    * id07
    
    * id07 WordFreq fix
    
    * id07 WordFreq fix2
    
    * fix conflicts
    
    * id08 add Exceptions
    
    * id08 add NonNull
    
    * id08 Kryo fix
    
    * id08 add JTests
    
    * id08 fix checkstyle
    
    * (8) Romanova (#258)
    
    * fix conflicts
    
    * id08 add Exceptions
    
    * id08 add NonNull
    
    * id08 Kryo fix
    
    * id08 add JTests
    
    * id08 fix checkstyle
    
    * id09
    
    * Squash 22.05
    
    * id06-07
    
    * id06-07 build
    
    * id06-07 build2
    
    * id06-07 checkstyle
    
    * id06 checkstyle
    
    * id07 fix reviews
    
    * id07 fix CountImpl mistakes
    
    * id07 fix CountImpl&DifWords mistakes
    
    * id07 fix DifWords mistakes
    
    * id07 fix DifWords mistakes2
    
    * id07 fix DifWords mistakes3
    
    * id07 fix CountWords mistakes
    
    * id07
    
    * id07 WordFreq fix
    
    * id07 WordFreq fix2
    
    * (8) Romanova (#258)
    
    * fix conflicts
    
    * id08 add Exceptions
    
    * id08 add NonNull
    
    * id08 Kryo fix
    
    * id08 add JTests
    
    * id08 fix checkstyle
    
    * id09 add NonNull & stylefix
    
    * id09 add isEmpty
    
    * id09 try to fix kryo
    
    * id09 try to fix2
    
    * id09
    
    * id09 add annotation
    
    * id09 change constructor
    
    * id09 fix mistake
    
    * id09 del super
    
    * id09 add CitySerlztn
    
    * id09 com mist
    
    * id09 CitySerlz
    
    * id09 CitySerlz2
    
    * id09 1 more time
    
    * id09 smth
    
    * id10 1st try
    
    * id10 Scalable start
    
    * id10 fix FixedTP&part ScalableTP after reviews
    
    * id10 add finish & change names(?)
    
    * id10 add FTP tests
    
    * id10 checkstyle
    
    * id10 add some tests
    
    * id10 some fixes

---
+++ solutions/src/main/java/ru/sberbank/school/task10/FixedThreadPool.java
@@ -0,0 +1,119 @@
+package ru.sberbank.school.task10;
+
+import lombok.NonNull;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+//FixedThreadPool - Количество потоков задается в конструкторе и не меняется.
+// (Должен быть конструктор с одним параметром типа int)
+public class FixedThreadPool implements ThreadPool {
+    private int countOfThreads;
+    private volatile boolean finish;
+    private ArrayList<Thread> threads;
+    private LinkedList<Runnable> tasks;
+
+    public FixedThreadPool(@NonNull int countOfThreads) {
+        if (countOfThreads <= 0) {
+            throw new IllegalArgumentException();
+        }
+        this.countOfThreads = countOfThreads;
+        threads = new ArrayList<>(countOfThreads);
+        tasks = new LinkedList<>();
+    }
+
+    /**
+     * Запускает потоки, которыые бездействуют, до тех пор пока не появится новое задание в очереди (см. execute)
+     */
+    @Override
+    public void start() {
+        for (int i = 0; i < countOfThreads; i++) {
+            threads.add(new ThreadWorker(i));
+            threads.get(i).start();
+        }
+    }
+
+    /**
+     * Отменяет запланированные задачи, останавливает запущенные задачи,
+     * останавливает потоки, переходит в начальное состояние.
+     */
+    @Override
+    public void stopNow() {
+        synchronized (tasks) {
+            tasks.clear();
+            finish = true;
+        }
+        synchronized (threads) {
+            for (Thread t : threads) {
+                if (!t.isInterrupted()) {
+                    t.interrupt();
+                }
+            }
+        }
+
+    }
+
+    /**
+     * Складывает задание в очередь. Освободившийся поток должен выполнить это задание.
+     * Каждое задание должны быть выполнено ровно 1 раз
+     *
+     * @param runnable Задача для выполнения
+     */
+    @Override
+    public void execute(@NonNull Runnable runnable) {
+        if (finish) {
+            throw new RuntimeException("Work is already finished");
+        }
+        synchronized (tasks) {
+            tasks.addLast(runnable);
+            tasks.notify();
+        }
+    }
+
+    public boolean checkThreads() throws IllegalStateException {
+        for (Thread t : threads) {
+            Thread.State currState = t.getState();
+            //System.out.println(currState);
+            if (currState == Thread.State.RUNNABLE)  {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private class ThreadWorker extends Thread {
+        private String name;
+
+        ThreadWorker(int i) {
+            name = "ThreadPoolWorker-" + i;
+        }
+
+        public void run() {
+            Runnable task;
+            while (!Thread.interrupted()) {
+                synchronized (tasks) {
+                    while (tasks.isEmpty()) {
+                        try {
+                            tasks.wait();
+                        } catch (InterruptedException e) {
+                            return;
+                        }
+                    }
+                    System.out.println(Thread.currentThread().getName() + " is running");
+                    task = tasks.removeFirst();
+                }
+                try {
+                    task.run();
+                } catch (RuntimeException e) {
+                    e.printStackTrace();
+                }
+
+            }
+
+        }
+
+    }
+}
+
+
+
---
+++ solutions/src/main/java/ru/sberbank/school/task10/ScalableThreadPool.java
@@ -0,0 +1,143 @@
+package ru.sberbank.school.task10;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+//ScalableThreadPool в конструкторе задается минимальное и максимальное(int min, int max) число потоков,
+//        количество запущенных потоков может быть увеличено от минимального к максимальному,
+//        если при добавлении нового задания в очередь нет свободного потока для исполнения этого задания.
+//        При отсутствии задания в очереди, количество потоков опять должно быть уменьшено до значения min.
+//        (Должен быть конструктор с двумя параметрами типа int)
+public class ScalableThreadPool implements ThreadPool {
+    private int maxCountThreads;
+    private int minCountThreads;
+    private volatile boolean finish;
+    private volatile int freeThreads;
+    private ArrayList<Thread> threads;
+    private LinkedList<Runnable> tasks;
+
+    public ScalableThreadPool(int min, int max) {
+        if (max < min || min <= 0) {
+            throw new IllegalArgumentException();
+        }
+        maxCountThreads = max;
+        minCountThreads = min;
+        freeThreads = min;
+        threads = new ArrayList<>(minCountThreads);
+        tasks = new LinkedList<>();
+    }
+
+    /**
+     * Запускает потоки, которыые бездействуют, до тех пор пока не появится новое задание в очереди (см. execute)
+     */
+    @Override
+    public void start() {
+        for (int i = 0; i < minCountThreads; i++) {
+            threads.add(new ThreadWorker(i, ""));
+            threads.get(i).start();
+        }
+    }
+
+    /**
+     * Отменяет запланированные задачи, останавливает запущенные задачи, останавливает потоки,
+     * переходит в начальное состояние.
+     */
+    @Override
+    public void stopNow() {
+        synchronized (tasks) {
+            tasks.clear();
+            finish = true;
+        }
+        for (Thread t : threads) {
+            if (!t.isInterrupted()) {
+                t.interrupt();
+            }
+        }
+        freeThreads = minCountThreads;
+    }
+
+    public boolean checkScalableThreads() throws IllegalStateException {
+        for (Thread t : threads) {
+            Thread.State currState = t.getState();
+            //System.out.println(currState);
+            if (currState == Thread.State.RUNNABLE) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * Складывает задание в очередь. Освободившийся поток должен выполнить это задание.
+     * Каждое задание должны быть выполнено ровно 1 раз
+     *
+     * @param runnable Задача для выполнения
+     */
+    @Override
+    public void execute(Runnable runnable) {
+        if (finish) {
+            throw new RuntimeException("Work is already finished");
+        }
+        synchronized (tasks) {
+            if ((threads.size() < maxCountThreads) && (freeThreads <= 0)) {
+                if (threads.get(threads.size() - 1).getName() == ("ThreadPoolWorker-" + maxCountThreads)) {
+                    threads.add(new ThreadWorker(threads.size(), "m"));
+                } else {
+                    threads.add(new ThreadWorker(threads.size(), ""));
+                }
+
+                threads.get(threads.size() - 1).start();
+            } else if ((threads.size() > minCountThreads) && tasks.isEmpty()) {
+                while (threads.size() > minCountThreads) {
+                    threads.remove(0);
+                }
+                freeThreads = minCountThreads + 1;
+            }
+            freeThreads--;
+//            if (freeThreads > 0) {
+//                freeThreads--;
+//            }
+            tasks.addLast(runnable);
+            tasks.notify();
+        }
+    }
+
+    public void checkThreads() throws IllegalStateException {
+        for (Thread t : threads) {
+            Thread.State currState = t.getState();
+            System.out.println(currState);
+        }
+    }
+
+    private class ThreadWorker extends Thread {
+        String name;
+
+        ThreadWorker(int i, String letter) {
+            name = "ThreadPoolWorker-" + letter + i;
+        }
+
+        public void run() {
+            Runnable task;
+            while (!Thread.interrupted()) {
+                synchronized (tasks) {
+                    while (tasks.isEmpty()) {
+                        try {
+                            tasks.wait();
+                        } catch (InterruptedException e) {
+                            return;
+                        }
+                    }
+                    System.out.println(Thread.currentThread().getName() + " is running..");
+                    task = tasks.removeFirst();
+                }
+                try {
+                    task.run();
+                } catch (RuntimeException e) {
+                    e.printStackTrace();
+                }
+            }
+
+        }
+
+    }
+}
