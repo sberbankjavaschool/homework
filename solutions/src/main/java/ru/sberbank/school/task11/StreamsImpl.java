70b09cb381610bec0baa3220856a9d89c6d24933
Dionisii Iuzhakov <dionisii.iuzhakov@t-systems.com>
2019-06-11 15:36:51 +0300

    Task11 implementation (Solution 12)

--- solutions/src/main/java/ru/sberbank/school/task11/StreamsImpl.java
+++ solutions/src/main/java/ru/sberbank/school/task11/StreamsImpl.java
@@ -2,7 +2,16 @@ package ru.sberbank.school.task11;
 
 import ru.sberbank.school.util.Solution;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Set;
 import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.stream.Stream;
@@ -11,14 +20,20 @@ import java.util.stream.Stream;
  */
 @Solution(12)
 public class StreamsImpl<T> {
+    private final List<T> items;
+
+    private StreamsImpl(List<T> items) {
+        this.items = items;
+    }
 
     /** Принимает на вход массив элементов и возвращает стрим построенный на основе этих элементов.
      *
      * @param elements входные элементы
      * @return стрим элементов из elements
      */
+    @SafeVarargs
     public static <T> StreamsImpl<T> of(T... elements) {
-        return null;
+        return new StreamsImpl<>(Arrays.asList(elements));
     }
 
     /** Принимает на вход коллекцию элементов и возвращает стрим построенный на основе этих элементов.
@@ -27,7 +42,8 @@ public class StreamsImpl<T> {
      * @return стрим элементов из elements
      */
     public static <T> StreamsImpl<T> of(Collection<T> elements) {
-        return null;
+        return new StreamsImpl<>(new ArrayList<>(elements));
+
     }
 
     /** Фильтрация элементов по заданному правилу. Необходимо подобрать нужый интерфейс для передачи в этот метод.
@@ -42,7 +58,13 @@ public class StreamsImpl<T> {
      * @return стрим
      */
     public StreamsImpl<T> filter(Predicate<? super T> object) {
-        return null;
+        List<T> resultList = new ArrayList<>();
+        for (T element : items) {
+            if (object.test(element)) {
+                resultList.add(element);
+            }
+        }
+        return new StreamsImpl<>(resultList);
     }
 
     /** Преобразование элементов в какие-то другие элементы.
@@ -58,7 +80,11 @@ public class StreamsImpl<T> {
      * @return стрим
      */
     public <R> StreamsImpl<R> transform(Function<? super T, ? extends R> object) {
-        return null;
+        List<R> resultList = new ArrayList<>();
+        for (T element : items) {
+            resultList.add(object.apply(element));
+        }
+        return new StreamsImpl<>(resultList);
     }
 
     /** Сортировка элементов. Необходимо подобрать нужый интерфейс для передачи в этот метод.
@@ -73,7 +99,9 @@ public class StreamsImpl<T> {
      * @return стрим
      */
     public StreamsImpl<T> sorted(Comparator<? super T> object) {
-        return null;
+        List<T> sortedList = new ArrayList<>(items);
+        sortedList.sort(object);
+        return new StreamsImpl<>(sortedList);
     }
 
     /** Преобразование стрима в Map. Необходимо подобрать нужные интерфейсы для передачи в этот метод.
@@ -88,7 +116,11 @@ public class StreamsImpl<T> {
      */
     public <K, V> Map<K, V> toMap(Function<? super T, ? extends K> keyMapper,
                                     Function<? super T, ? extends V> valueMapper) {
-        return null;
+        Map<K, V> map = new HashMap<>();
+        for (T element : items) {
+            map.put(keyMapper.apply(element), valueMapper.apply(element));
+        }
+        return map;
     }
 
     /** Преобразование стрима в Set.
@@ -101,7 +133,7 @@ public class StreamsImpl<T> {
      * @return Set элементов
      */
     public Set<T> toSet() {
-        return null;
+        return new HashSet<>(items);
     }
 
     /** Преобразование стрима в List.
