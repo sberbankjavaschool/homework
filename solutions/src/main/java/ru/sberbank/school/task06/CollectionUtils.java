c3c2ce3dc8dbb11cf867fcd202af0929732390da
Egor Bzhosko <lansxkeku@mail.ru>
2019-06-11 23:41:26 +0300

    id:6 temp

---
+++ solutions/src/main/java/ru/sberbank/school/task06/CollectionUtils.java
@@ -0,0 +1,143 @@
+package ru.sberbank.school.task06;
+
+import java.util.*;
+
+public class CollectionUtils {
+
+    /**
+     * Добавление всех элементов одной коллекции в другую.
+     *
+     * @param source      источник
+     * @param destination потребитель
+     */
+    public static <T> void addAll(List<? extends T> source, List<? super T> destination) {
+        destination.addAll(source);
+    }
+
+    /**
+     * Инициализация листа с реализаций java.util.ArrayList.
+     *
+     * @return пустой ArrayList
+     */
+    public static <T> List<T> newArrayList() {
+        return new ArrayList<>();
+    }
+
+    /**
+     * Поиск индекса элемента в списке.
+     *
+     * @param source список, в котором искать элемент
+     * @param o      элемент
+     * @return индекс элемента
+     */
+    public static <T> int indexOf(List<? extends T> source, Object o) {
+        return source.indexOf(o);
+    }
+
+    /**
+     * Обрезание списка заданным числом элементов. limit({1, 2 ,3}, 2) => {1, 2}
+     *
+     * @param source список
+     * @param size   необходимое число элементов
+     * @return сокращенный список
+     */
+    public static List limit(List source, int size) {
+        return source.subList(0, size);
+    }
+
+    /**
+     * Добавление элемента в коллекцию.
+     *
+     * @param dest коллекция, в которую необходимо вставить элемент
+     * @param o    элемент
+     */
+    public static <T> void add(List<? super T> dest, T o) {
+        dest.add(o);
+    }
+
+
+    /**
+     * Удаление всеъх элементов одной коллекии из другой.
+     *
+     * @param removeFrom коллекция, из которой удалять элементы
+     * @param toRemove   коллекция, в которой лежат элементы для удаления из коллекции removeFrom
+     */
+    public static <T> void removeAll(List<? super T> removeFrom, List<? extends T> toRemove) {
+        removeFrom.removeAll(toRemove);
+    }
+
+    /**
+     * Проверка на содержение одной коллекции в другой.
+     *
+     * @param c1 первая коллекция
+     * @param c2 вторая коллекция
+     * @return true, если все элементы коллекции c2 содержатся в c1
+     */
+    public static <T> boolean containsAll(List<? super T> c1, List<? extends T> c2) {
+        return c1.containsAll(c2);
+    }
+
+    /**
+     * Проверка на содержение хотя бы одного элемента одной коллекции в другой.
+     *
+     * @param c1 первая коллекция
+     * @param c2 вторая коллекция
+     * @return true, если хотя бы 1 элемент коллекции c2 содержатся в c1
+     */
+    public static <T> boolean containsAny(List<? super T> c1, List<? extends T> c2) {
+        for (Object o : c1) {
+            for (T t : c2) {
+                if (o.equals(t)) {
+                    return true;
+                }
+
+            }
+        }
+        return false;
+    }
+
+    /**
+     * Создание коллекции, содержащей элементы из входной коллекции в диапазоне от min до max (не
+     * индексы, а значения). Элементы необходимо сравнивать, через интерфейс Comparable Пример:
+     * range(Arrays.asList(8, 1, 3, 5, 6, 4), 3, 6) вернет {3,4,5,6}
+     *
+     * @param list исходная коллекция
+     * @param min  минимальное значение
+     * @param max  максимальнео значение
+     * @return отфильрованный по минимальному и максимальному значению список
+     */
+    public static <T extends Comparable<? super T>> List<T> range(List<? extends T> list, T min, T max) {
+        TreeSet<T> set = new TreeSet<T>() {{
+            for (T t : list) {
+                if (t.compareTo(min) >= 0 && t.compareTo(max) <= 0) {
+                    add(t);
+                }
+            }
+        }};
+
+        return new ArrayList<>(set);
+    }
+
+    /**
+     * Создание коллекции, содержащей элементы из входной коллекции в диапазоне от min до max (не
+     * индексы, а значения). Элементы необходимо сравнивать, через интерфейс Comparator Пример:
+     * range(Arrays.asList(8, 1, 3, 5, 6, 4), 3, 6) вернет {3,4,5,6}
+     *
+     * @param list исходная коллекция
+     * @param min  минимальное значение
+     * @param max  максимальнео значение
+     * @return отфильрованный по минимальному и максимальному значению список
+     */
+    public static <T> List<T> range(List<? extends T> list, T min, T max, Comparator<T> comparator) {
+        TreeSet<T> set = new TreeSet<T>() {{
+            for (T t : list) {
+                if (comparator.compare(t, min) >= 0 && comparator.compare(t, max) <= 0) {
+                    add(t);
+                }
+            }
+
+        }};
+
+        return new ArrayList<>(set);
+    }
+}
---
+++ solutions/src/main/java/ru/sberbank/school/task06/CountMapImpl.java
@@ -0,0 +1,57 @@
+package ru.sberbank.school.task06;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class CountMapImpl<T> implements CountMap<T> {
+
+    private final Map<T, Integer> map;
+
+    public CountMapImpl() {
+        this.map = new HashMap<>();
+    }
+
+    @Override
+    public void add(T o) {
+        Integer count = map.get(o);
+        map.put(o, count == null ? 1 : count + 1);
+    }
+
+    public int getCount(T o) {
+        return map.get(o);
+    }
+
+    @Override
+    public int remove(T o) {
+        return map.remove(o);
+    }
+
+    @Override
+    public int size() {
+        return map.size();
+    }
+
+    @Override
+    public void addAll(CountMap<? extends T> source) {
+        for (T t : source.toMap().keySet()) {
+            add(t);
+        }
+    }
+
+    @Override
+    public Map<T, Integer> toMap() {
+        return new HashMap<>(map);
+    }
+
+    @Override
+    public void toMap(Map<? super T, Integer> destination) {
+        destination = new HashMap<>(map);
+    }
+
+    @Override
+    public String toString() {
+        return "CountMapImpl{" +
+                "map=" + map.toString() +
+                '}';
+    }
+}
