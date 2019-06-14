42a370de09976fb19f46afcf4fee4d10be1a77c5
Egor Bzhosko <lansxkeku@mail.ru>
2019-06-14 18:00:13 +0300

    id:11 first check

---
+++ solutions/src/main/java/ru/sberbank/school/task11/Streams.java
@@ -0,0 +1,145 @@
+package ru.sberbank.school.task11;
+
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+/**
+ * Облегченная версия класса {@link Stream}
+ */
+public class Streams<T> {
+
+    private List<T> list;
+
+    @SafeVarargs
+    public Streams(T... list) {
+        this.list = new ArrayList<>(Arrays.asList(list));
+    }
+
+    public Streams(Collection<T> list) {
+        this.list = new ArrayList<>(list);
+    }
+
+    /**
+     * Принимает на вход массив элементов и возвращает стрим построенный на основе этих элементов.
+     *
+     * @param elements входные элементы
+     * @return стрим элементов из elements
+     */
+    public static <T> Streams of(T... elements) {
+        return new Streams<>(elements);
+    }
+
+    /**
+     * Принимает на вход коллекцию элементов и возвращает стрим построенный на основе этих элементов.
+     *
+     * @param elements входные элементы
+     * @return стрим элементов из elements
+     */
+    public static <T> Streams of(Collection<T> elements) {
+        return new Streams<>(elements);
+    }
+
+    /**
+     * Фильтрация элементов по заданному правилу. Необходимо подобрать нужый интерфейс для передачи в этот метод.
+     * <p>
+     * Иными словами, этот метод оставляет в коллекции только те элементы, которые удовлетворяют условию в лямбде.
+     * <p>
+     * Аналог Stream#map(Function).
+     * <p>
+     * Intermediate операция.
+     *
+     * @param predicate - правило фильтрации элементов.
+     * @return стрим
+     */
+    public Streams<T> filter(Predicate<? super T> predicate) {
+
+        return new Streams<>(list.stream()
+                .filter(predicate)
+                .collect(Collectors.toList()));
+    }
+
+    /**
+     * Преобразование элементов в какие-то другие элементы. Необходимо подобрать нужый интерфейс для передачи в этот метод.
+     * <p>
+     * Например, данный метод должен уметь извлекать какие-то поля из объектов исходного типа (если это возможно).
+     * <p>
+     * Аналог Stream#map.
+     * <p>
+     * Intermediate операция.
+     *
+     * @param function - правило траснформации элементов.
+     * @return стрим
+     */
+    public <R> Streams<R> transform(Function<? super T, ? extends R> function) {
+        return new Streams<>(list.stream()
+                .map(function)
+                .collect(Collectors.toList()));
+    }
+
+    /**
+     * Сортировка элементов. Необходимо подобрать нужый интерфейс для передачи в этот метод.
+     * <p>
+     * Данный метод должен уметь сортировать элементы по заданному правилу.
+     * <p>
+     * Аналог Stream#sorted
+     * <p>
+     * Intermediate операция.
+     *
+     * @param comparator - правило сортировки элементов.
+     * @return стрим
+     */
+    public Streams<T> sorted(Comparator<? super T> comparator) {
+        return new Streams<>(list.stream()
+                .sorted(comparator)
+                .collect(Collectors.toList()));
+    }
+
+    /**
+     * Преобразование стрима в Map. Необходимо подобрать нужные интерфейсы для передачи в этот метод.
+     * <p>
+     * Данный метод должен уметь преобразовать стрим в Map используя keyMapper и valueMapper.
+     * <p>
+     * Terminate операция.
+     *
+     * @param keyMapper   - правило создания ключа.
+     * @param valueMapper - правило создания значения.
+     * @return Map, собранная по правилам keyMapper и valueMapper
+     */
+    public <K,V> Map<K,V> toMap(Function<? super T, ? extends K> keyMapper, Function<? super T, ? extends V> valueMapper) {
+        return list.stream()
+                .collect(Collectors.toMap(keyMapper, valueMapper));
+    }
+
+    /**
+     * Преобразование стрима в Set.
+     * <p>
+     * Данный метод должен уметь преобразовать стрим в Set. Обратите внимание, что метод sorted должен учитываться
+     * при вызове этого метода.
+     * <p>
+     * Terminate операция.
+     *
+     * @return Set элементов
+     */
+    public Set<T> toSet() {
+        return new HashSet<>(list);
+    }
+
+    /**
+     * Преобразование стрима в List.
+     * <p>
+     * Данный метод должен уметь преобразовать стрим в List.
+     * <p>
+     * Terminate операция.
+     *
+     * @return List элементов
+     */
+    public List<T> toList() {
+        return new ArrayList<>(list);
+    }
+
+}
