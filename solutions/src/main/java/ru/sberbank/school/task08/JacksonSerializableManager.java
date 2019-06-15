c03140a33bb33c3c2612291b91ad23dbcfbfbeee
Egor Bzhosko <lansxkeku@mail.ru>
2019-06-15 16:29:14 +0300

    id:8 for check

---
+++ solutions/src/main/java/ru/sberbank/school/task08/JacksonSerializableManager.java
@@ -0,0 +1,76 @@
+package ru.sberbank.school.task08;
+
+import com.esotericsoftware.kryo.io.Input;
+import com.esotericsoftware.kryo.io.Output;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.NonNull;
+import ru.sberbank.school.task08.state.GameObject;
+import ru.sberbank.school.task08.state.InstantiatableEntity;
+import ru.sberbank.school.task08.state.MapState;
+import ru.sberbank.school.task08.state.Savable;
+import ru.sberbank.school.util.Solution;
+
+import java.io.*;
+
+import java.util.List;
+
+@Solution(8)
+public class JacksonSerializableManager extends SaveGameManager {
+
+
+    /**
+     * Класс-наследник должен иметь конструктор эквивалентный этому.
+     *
+     * @param filesDirectoryPath Путь до директории, в которой хранятся файлы сохранений
+     */
+    public JacksonSerializableManager(@NonNull String filesDirectoryPath) {
+        super(filesDirectoryPath);
+    }
+
+
+    @Override
+    public void initialize() {
+
+    }
+
+
+    @Override
+    public void saveGame(String filename, Savable gameState) throws SaveGameException {
+        try (OutputStream outputStream = new FileOutputStream(filesDirectory + filename);
+               Output output = new Output(outputStream)) {
+           new ObjectMapper().writeValue(output, gameState);
+        } catch (FileNotFoundException ex) {
+            throw new SaveGameException("File not found", ex, SaveGameException.Type.USER, gameState);
+        } catch (IOException ex) {
+            throw new SaveGameException("I/O operation has been failed", ex, SaveGameException.Type.IO, gameState);
+        }
+    }
+
+    @Override
+    public Savable loadGame(String filename) throws SaveGameException {
+
+        MapState mapState = null;
+
+        try (InputStream inputStream = new FileInputStream(filesDirectory + filename);
+               Input input = new Input(inputStream)) {
+           mapState = new ObjectMapper().readValue(input, MapState.class);
+        } catch (FileNotFoundException ex) {
+            throw new SaveGameException("File not found", ex, SaveGameException.Type.USER, mapState);
+        } catch (IOException ex) {
+            throw new SaveGameException("I/O operation has been failed", ex, SaveGameException.Type.IO, mapState);
+        }
+        return mapState;
+    }
+
+    @Override
+    public InstantiatableEntity createEntity(InstantiatableEntity.Type type,
+                                             InstantiatableEntity.Status status,
+                                             long hitPoints) {
+        return new GameObject(type, status, hitPoints);
+    }
+
+    @Override
+    public Savable createSavable(String name, List entities) {
+        return new MapState<GameObject>(name, entities);
+    }
+}
---
+++ solutions/src/main/java/ru/sberbank/school/task08/kryo/MapStateSerializer.java
@@ -0,0 +1,34 @@
+package ru.sberbank.school.task08.kryo;
+
+import com.esotericsoftware.kryo.Kryo;
+import com.esotericsoftware.kryo.Serializer;
+import com.esotericsoftware.kryo.io.Input;
+import com.esotericsoftware.kryo.io.Output;
+import ru.sberbank.school.task08.state.GameObject;
+import ru.sberbank.school.task08.state.InstantiatableEntity;
+import ru.sberbank.school.task08.state.MapState;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class MapStateSerializer extends Serializer<MapState<GameObject>> {
+    @Override
+    public void write(Kryo kryo, Output output, MapState<GameObject> object) {
+        output.writeString(object.getName());
+
+        kryo.writeClassAndObject(output, object.getGameObjects());
+
+    }
+
+    @Override
+    public MapState read(Kryo kryo, Input input, Class<? extends MapState<GameObject>> type) {
+
+        String name = input.readString();
+
+        @SuppressWarnings("unchecked")
+        List<GameObject> list = (List<GameObject>) kryo.readClassAndObject(input);
+
+        return new MapState(name, list);
+    }
+}
