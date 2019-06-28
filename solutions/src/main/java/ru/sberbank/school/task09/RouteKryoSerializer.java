c6873be5cdb2fe54d4742b046ca46c5bceb51e09
Sergey <Ganzalo2010@gmail.com>
2019-06-28 17:07:31 +0300

    dz 9 in process

--- solutions/src/main/java/ru/sberbank/school/task09/RouteKryoSerializer.java
+++ solutions/src/main/java/ru/sberbank/school/task09/RouteKryoSerializer.java
@@ -1,4 +1,19 @@
 package ru.sberbank.school.task09;
 
-public class RouteKryoSerializer {
+import com.esotericsoftware.kryo.Kryo;
+import com.esotericsoftware.kryo.Serializer;
+import com.esotericsoftware.kryo.io.Input;
+import com.esotericsoftware.kryo.io.Output;
+
+public class RouteKryoSerializer extends Serializer<Route<City>> {
+
+    @Override
+    public void write(Kryo kryo, Output output, Route<City> object) {
+
+    }
+
+    @Override
+    public Route<City> read(Kryo kryo, Input input, Class<? extends Route<City>> type) {
+        return null;
+    }
 }
--- solutions/src/main/java/ru/sberbank/school/task09/RouteServiceReal.java
+++ solutions/src/main/java/ru/sberbank/school/task09/RouteServiceReal.java
@@ -1,9 +1,23 @@
 package ru.sberbank.school.task09;
 
+import com.esotericsoftware.kryo.Kryo;
+
 import java.time.LocalDate;
 import java.util.List;
 
+@Solution(9)
 public class RouteServiceReal extends RouteService<City, Route<City>> {
+
+    private Kryo kryo;
+
+    public RouteServiceReal(@NonNull String path) {
+        super(path);
+        this.kryo = new Kryo();
+        kryo.register(Route.class, new RouteKryoSerializer());
+        kryo.register(City.class, new CityKryoSerializer());
+    }
+
+
     @Override
     public Route getRoute(String from, String to) {
         return null;
