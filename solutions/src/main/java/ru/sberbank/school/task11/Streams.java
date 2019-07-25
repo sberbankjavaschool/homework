956851b60347972e6c3d45cbda7bdeb3c063c14a
Sergey <Ganzalo2010@gmail.com>
2019-07-25 19:01:48 +0300

    dz 11 done

--- solutions/src/main/java/ru/sberbank/school/task11/Streams.java
+++ solutions/src/main/java/ru/sberbank/school/task11/Streams.java
@@ -3,6 +3,7 @@ package ru.sberbank.school.task11;
 import lombok.NonNull;
 
 import java.util.*;
+import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.function.Supplier;
 import java.util.stream.Collectors;
@@ -14,7 +15,7 @@ import java.util.stream.Stream;
 public class Streams<T> {
 
     private final List<Supplier<List<T>>> suppliers = new ArrayList<>();
-    private final List<T> elements = new ArrayList<>();
+    private List<T> elements = new ArrayList<>();
 
     private Streams(Collection<T> elements) {
         this.elements.addAll(elements);
@@ -56,7 +57,6 @@ public class Streams<T> {
         suppliers.add(() -> elements.stream()
                 .filter(object)
                 .collect(Collectors.toList()));
-
         return this;
     }
 
@@ -72,8 +72,19 @@ public class Streams<T> {
      * @param object - правило траснформации элементов.
      * @return стрим
      */
-    public Streams transform(Object object) {
-        return null;
+    @SuppressWarnings("unchecked")
+    public <R> Streams<R> transform(Function<? super T, ? extends R> object) {
+
+        List<R> temp = new ArrayList<>();
+
+        suppliers.add(() -> {
+            temp.addAll(elements.stream()
+                    .map(object)
+                    .collect(Collectors.toList()));
+            return (List<T>) temp;
+        });
+
+        return (Streams<R>) this;
     }
 
     /**
@@ -88,8 +99,11 @@ public class Streams<T> {
      * @param object - правило сортировки элементов.
      * @return стрим
      */
-    public Streams sorted(Object object) {
-        return null;
+    public Streams<T> sorted(Comparator<? super T>  object) {
+        suppliers.add(() -> elements.stream()
+                .sorted(object)
+                .collect(Collectors.toList()));
+        return this;
     }
 
     /**
@@ -103,8 +117,9 @@ public class Streams<T> {
      * @param valueMapper - правило создания значения.
      * @return Map, собранная по правилам keyMapper и valueMapper
      */
-    public Map toMap(Object keyMapper, Object valueMapper) {
-        return null;
+    public <K, V> Map<K, V> toMap(Function<? super T, ? extends K> keyMapper,
+                                  Function<? super T, ? extends V> valueMapper) {
+        return terminate().stream().collect(Collectors.toMap(keyMapper, valueMapper));
     }
 
     /**
@@ -117,8 +132,8 @@ public class Streams<T> {
      *
      * @return Set элементов
      */
-    public Set toSet() {
-        return null;
+    public Set<T> toSet() {
+        return new LinkedHashSet<>(terminate());
     }
 
     /**
@@ -130,8 +145,14 @@ public class Streams<T> {
      *
      * @return List элементов
      */
-    public List toList() {
-        return null;
+    public List<T> toList() {
+        return new ArrayList<>(terminate());
     }
 
+    private List<T> terminate() {
+        for (Supplier<List<T>> supplier : suppliers) {
+            elements = supplier.get();
+        }
+        return elements;
+    }
 }
