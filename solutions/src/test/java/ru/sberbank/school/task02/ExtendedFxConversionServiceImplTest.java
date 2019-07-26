fd7d65fe03cda2ef353cdf9bb985fed57c086b6d
Vitaly Martyanov <vitalkamart@gmail.com>
2019-07-26 11:33:27 +0300

    wrong args test

--- solutions/src/test/java/ru/sberbank/school/task02/ExtendedFxConversionServiceImplTest.java
+++ solutions/src/test/java/ru/sberbank/school/task02/ExtendedFxConversionServiceImplTest.java
@@ -1,6 +1,8 @@
 package ru.sberbank.school.task02;
 
+import org.junit.Ignore;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import ru.sberbank.school.task02.util.*;
 
@@ -16,15 +18,17 @@ class ExtendedFxConversionServiceImplTest {
     private static ExternalQuotesService quotesService;
     private static ExtendedFxConversionService converter;
     private static Symbol symbol;
+    private static Beneficiary beneficiary;
 
     @BeforeAll
     static void init() {
         quotesService = new ExternalQuotesServiceDemo();
         converter = new ExtendedFxConversionServiceImpl(quotesService);
         symbol = Symbol.USD_RUB;
-
+        beneficiary = Beneficiary.BANK;
     }
 
+    @Ignore
     @Test
     void test1() {
         Quote q = quotesService.getQuotes(symbol).get(0);
@@ -34,6 +38,7 @@ class ExtendedFxConversionServiceImplTest {
         System.out.println("==========================================");
     }
 
+    @Ignore
     @Test
     void quoteVolumes() {
         quotesService.getQuotes(symbol)
@@ -43,6 +48,7 @@ class ExtendedFxConversionServiceImplTest {
                         + "     offer " + BigDecimal.valueOf(1).divide(quote.getOffer(),10,   RoundingMode.HALF_UP)));
     }
 
+    @Ignore
     @Test
     void test() {
 
@@ -54,9 +60,21 @@ class ExtendedFxConversionServiceImplTest {
         amounts.add(BigDecimal.valueOf(100000));
         amounts.add(BigDecimal.valueOf(10000000));
 
-        Beneficiary beneficiary = Beneficiary.BANK;
         ClientOperation operation = ClientOperation.BUY;
         amounts.forEach(i -> System.out.println(
                         converter.convertReversed(operation, symbol, i, beneficiary).orElse(BigDecimal.ZERO)));
+    }
+
+    @Test
+    @DisplayName("тест обработки неверных параметров метода")
+    void expectedExceptions() {
+        assertThrows(NullPointerException.class, () -> converter.convertReversed(
+                null, symbol, BigDecimal.valueOf(2000), beneficiary));
+        assertThrows(NullPointerException.class, () -> converter.convertReversed(
+                ClientOperation.SELL, null, BigDecimal.valueOf(2000), beneficiary));
+        assertThrows(NullPointerException.class, () -> converter.convertReversed(
+                ClientOperation.SELL, symbol, null, beneficiary));
+        assertThrows(NullPointerException.class, () -> converter.convertReversed(
+                ClientOperation.SELL, symbol, BigDecimal.valueOf(2000), null));
     }
 }
