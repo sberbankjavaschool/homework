6c07aea89b3a4a793bd8c75e6c111c208ab2a960
Vitaly Martyanov <vitalkamart@gmail.com>
2019-07-26 05:13:51 +0300

    reverse basic

--- solutions/src/main/java/ru/sberbank/school/task02/ServiceFactoryImpl.java
+++ solutions/src/main/java/ru/sberbank/school/task02/ServiceFactoryImpl.java
@@ -12,6 +12,6 @@ public class ServiceFactoryImpl implements ServiceFactory {
 
     @Override
     public ExtendedFxConversionService getExtendedFxConversionService(ExternalQuotesService externalQuotesService) {
-        return null;
+        return new ExtendedFxConversionServiceImpl(externalQuotesService);
     }
 }
